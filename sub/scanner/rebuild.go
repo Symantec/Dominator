d360d15bd850318b354dd7784d7d78990c6d875d
Richard Gooch <rg+github0@safe-mbox.com>
2015-07-09 23:37:09 -0700

    Rebuild pointers for regular files.

--- sub/scanner/rebuild.go
+++ sub/scanner/rebuild.go
@@ -5,12 +5,19 @@ func (fs *FileSystem) rebuildPointers() {
 }
 
 func (directory *Directory) rebuildPointers(fs *FileSystem) {
+	for _, file := range directory.RegularFileList {
+		file.rebuildPointers(fs)
+	}
 	for _, file := range directory.FileList {
 		file.rebuildPointers(fs)
 	}
 	for _, dir := range directory.DirectoryList {
 		dir.rebuildPointers(fs)
 	}
+}
+
+func (file *RegularFile) rebuildPointers(fs *FileSystem) {
+	file.inode = fs.RegularInodeTable[file.InodeNumber]
 }
 
 func (file *File) rebuildPointers(fs *FileSystem) {
