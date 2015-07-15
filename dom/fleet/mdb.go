a6f8eeac0174b3c9eace24911051d1c9ca13c757
Richard Gooch <rg+github0@safe-mbox.com>
2015-07-15 09:25:02 -0700

    Fix connection leak in dom.

--- dom/fleet/mdb.go
+++ dom/fleet/mdb.go
@@ -6,12 +6,12 @@ import (
 
 func (fleet *Fleet) mdbUpdate(mdb *mdb.Mdb) {
 	fleet.nextSubToScan = 0
-	fleet.subs = make([]Sub, 0, mdb.Len())
+	fleet.subs = make([]*Sub, 0, mdb.Len())
 	for _, machine := range mdb.Machines {
 		var sub Sub
 		sub.hostname = machine.Hostname
 		sub.requiredImage = machine.RequiredImage
 		sub.plannedImage = machine.PlannedImage
-		fleet.subs = append(fleet.subs, sub)
+		fleet.subs = append(fleet.subs, &sub)
 	}
 }
