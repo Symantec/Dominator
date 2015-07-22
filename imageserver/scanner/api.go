109759de9ffe2422cb3f40afa1fe9910199de7fe
Richard Gooch <rg+github0@safe-mbox.com>
2015-07-22 10:52:49 -0700

    Check in embyronic Filter, Directory, Image and Hash types.

--- imageserver/scanner/api.go
+++ imageserver/scanner/api.go
@@ -1,6 +1,21 @@
 package scanner
 
+type FilterEntry string
+
+type Filter []FilterEntry
+
+type Directory struct {
+}
+
+type Image struct {
+	filter       Filter
+	topDirectory *Directory
+}
+
+type Hash [64]byte
+
 type ImageDataBase struct {
+	imageMap map[string]*Image
 }
 
 func LoadImageDataBase(baseDir string) (*ImageDataBase, error) {
