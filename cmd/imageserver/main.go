1620e369fea83b338f83eb58a9d9d90c8558ab38
Richard Gooch <rg+github0@safe-mbox.com>
2015-07-21 19:02:29 -0700

    Check in embryonic image server.

---
+++ cmd/imageserver/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var (
+	debug   = flag.Bool("debug", false, "If true, show debugging output")
+	portNum = flag.Uint("portNum", 6971,
+		"Port number to allocate and listen on for HTTP/RPC")
+	dataDir = flag.String("stateDir", "/var/lib/imageserver",
+		"Name of image server data directory.")
+)
+
+func main() {
+	flag.Parse()
+	if os.Geteuid() == 0 {
+		fmt.Println("Do not run the Image Server as root")
+		os.Exit(1)
+	}
+	fi, err := os.Lstat(*dataDir)
+	if err != nil {
+		fmt.Printf("Cannot stat: %s\t%s\n", *dataDir, err)
+		os.Exit(1)
+	}
+	if !fi.IsDir() {
+		fmt.Printf("%s is not a directory\n", *dataDir)
+		os.Exit(1)
+	}
+}
