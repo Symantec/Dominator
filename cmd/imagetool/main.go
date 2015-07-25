bd6b73825adf756d7aba2ba8a0552a0e21d716dc
Richard Gooch <rg+github0@safe-mbox.com>
2015-07-24 21:23:39 -0700

    Create addImage() skeleton.

--- cmd/imagetool/main.go
+++ cmd/imagetool/main.go
@@ -17,6 +17,20 @@ var (
 		"Port number of image server")
 )
 
+func addImage(name, imageFilename, filterFilename string) error {
+	imageFile, err := os.Open(imageFilename)
+	if err != nil {
+		return err
+	}
+	defer imageFile.Close()
+	filterFile, err := os.Open(filterFilename)
+	if err != nil {
+		return err
+	}
+	defer filterFile.Close()
+	return nil
+}
+
 func main() {
 	flag.Parse()
 	if flag.NArg() < 1 {
@@ -29,6 +43,11 @@ func main() {
 			fmt.Println(
 				"Usage: imagetool [flags...] add name imagefile filterfile")
 			os.Exit(2)
+		}
+		err := addImage(flag.Arg(1), flag.Arg(2), flag.Arg(3))
+		if err != nil {
+			fmt.Printf("Error adding image\t%s\n", err)
+			os.Exit(1)
 		}
 	case flag.Arg(0) == "delete":
 		if flag.NArg() != 2 {
