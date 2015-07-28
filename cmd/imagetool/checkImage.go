3e639f6f489ccc64ca18484c76b052a52fec1264
Richard Gooch <rg+github0@safe-mbox.com>
2015-07-27 19:13:13 -0700

    Implement imagetool.checkImage().

--- cmd/imagetool/checkImage.go
+++ cmd/imagetool/checkImage.go
@@ -2,20 +2,30 @@ package main
 
 import (
 	"fmt"
-	_ "github.com/Symantec/Dominator/proto/imageserver"
+	"github.com/Symantec/Dominator/proto/imageserver"
 	"net/rpc"
 	"os"
 )
 
 func checkImageSubcommand(client *rpc.Client, args []string) {
-	err := checkImage(client, args[0])
+	imageExists, err := checkImage(client, args[0])
 	if err != nil {
 		fmt.Printf("Error checking image\t%s\n", err)
 		os.Exit(1)
 	}
-	os.Exit(0)
+	if imageExists {
+		os.Exit(0)
+	}
+	os.Exit(1)
 }
 
-func checkImage(client *rpc.Client, name string) error {
-	return nil
+func checkImage(client *rpc.Client, name string) (bool, error) {
+	var request imageserver.CheckRequest
+	request.ImageName = name
+	var reply *imageserver.CheckResponse
+	err := client.Call("Imageserver.CheckImage", request, &reply)
+	if err != nil {
+		return false, err
+	}
+	return reply.ImageExists, nil
 }
