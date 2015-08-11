05b2bdabc4d175bbfc1d52cb326efca3c62675c8
Richard Gooch <rg+github0@safe-mbox.com>
2015-08-11 14:04:08 -0700

    Initialise imageMap.

--- imageserver/scanner/load.go
+++ imageserver/scanner/load.go
@@ -1,12 +1,14 @@
 package scanner
 
 import (
+	"github.com/Symantec/Dominator/lib/image"
 	"github.com/Symantec/Dominator/objectserver"
 )
 
 func loadImageDataBase(baseDir string, objSrv objectserver.ObjectServer) (
 	*ImageDataBase, error) {
 	imdb := new(ImageDataBase)
+	imdb.imageMap = make(map[string]*image.Image)
 	imdb.objectServer = objSrv
 	return imdb, nil
 }
