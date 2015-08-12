c3c9d60dd606087e43be69fac734752456f215b4
Richard Gooch <rg+github0@safe-mbox.com>
2015-08-11 21:56:20 -0700

    Add persistence to image server.

--- imageserver/scanner/imdb.go
+++ imageserver/scanner/imdb.go
@@ -1,8 +1,12 @@
 package scanner
 
 import (
+	"bufio"
+	"encoding/gob"
 	"errors"
 	"github.com/Symantec/Dominator/lib/image"
+	"os"
+	"path"
 )
 
 func (imdb *ImageDataBase) addImage(image *image.Image, name string) error {
@@ -11,8 +15,21 @@ func (imdb *ImageDataBase) addImage(image *image.Image, name string) error {
 	if _, ok := imdb.imageMap[name]; ok {
 		return errors.New("image: " + name + " already exists")
 	} else {
+		filename := path.Join(imdb.baseDir, name)
+		err := os.MkdirAll(path.Dir(filename), 0755)
+		if err != nil {
+			return err
+		}
+		file, err := os.Create(filename)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		writer := bufio.NewWriter(file)
+		defer writer.Flush()
+		encoder := gob.NewEncoder(writer)
+		encoder.Encode(image)
 		imdb.imageMap[name] = image
-		// TODO(rgooch): Write to persistent store.
 		return nil
 	}
 }
@@ -28,8 +45,12 @@ func (imdb *ImageDataBase) deleteImage(name string) error {
 	imdb.Lock()
 	defer imdb.Unlock()
 	if _, ok := imdb.imageMap[name]; ok {
+		filename := path.Join(imdb.baseDir, name)
+		err := os.Remove(filename)
+		if err != nil {
+			return err
+		}
 		delete(imdb.imageMap, name)
-		// TODO(rgooch): Write to persistent store.
 		return nil
 	} else {
 		return errors.New("image: " + name + " does not exist")
