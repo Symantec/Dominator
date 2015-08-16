1b4cd4aa6b0654f94595f19b4963f1a45c843a47
Richard Gooch <rg+github0@safe-mbox.com>
2015-08-16 10:42:45 -0700

    Show image summaries and deep links to details on imageserver web page.

--- imageserver/httpd/listImages.go
+++ imageserver/httpd/listImages.go
@@ -3,7 +3,11 @@ package httpd
 import (
 	"bufio"
 	"fmt"
+	"github.com/Symantec/Dominator/lib/format"
+	"github.com/Symantec/Dominator/lib/image"
+	"io"
 	"net/http"
+	"sort"
 )
 
 func listImagesHandler(w http.ResponseWriter, req *http.Request) {
@@ -13,9 +17,30 @@ func listImagesHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(writer, "<body>")
 	fmt.Fprintln(writer, "<h3>")
 	writeLinks(writer)
-	images := imageDataBase.ListImages()
-	for _, image := range images {
-		fmt.Fprintf(writer, "%s<br>\n", image)
+	imageNames := imageDataBase.ListImages()
+	sort.Strings(imageNames)
+	fmt.Fprintln(writer, "<table>")
+	fmt.Fprintln(writer, "  <tr>")
+	fmt.Fprintln(writer, "    <td><b>Name</b></td>")
+	fmt.Fprintln(writer, "    <td><b>Data Size</b></td>")
+	fmt.Fprintln(writer, "    <td><b>Data Inodes</b></td>")
+	fmt.Fprintln(writer, "    <td><b>Filter Lines</b></td>")
+	fmt.Fprintln(writer, "  </tr>")
+	for _, name := range imageNames {
+		showImage(writer, name, imageDataBase.GetImage(name))
 	}
+	fmt.Fprintln(writer, "</table>")
 	fmt.Fprintln(writer, "</body>")
+}
+
+func showImage(writer io.Writer, name string, image *image.Image) {
+	fmt.Fprintf(writer, "  <tr>\n")
+	fmt.Fprintf(writer, "    <td>%s</td>\n", name)
+	fmt.Fprintf(writer, "    <td><a href=\"listImage?%s\">%s</a></td>\n",
+		name, format.FormatBytes(image.FileSystem.TotalDataBytes))
+	fmt.Fprintf(writer, "    <td><a href=\"listImage?%s\">%d</a></td>\n",
+		name, len(image.FileSystem.RegularInodeTable))
+	fmt.Fprintf(writer, "    <td><a href=\"listFilter?%s\">%d</a></td>\n",
+		name, len(image.Filter.FilterLines))
+	fmt.Fprintf(writer, "  </tr>\n")
 }
