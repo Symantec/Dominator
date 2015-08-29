72a418c2928f055d484b59e80b2c1433ca0c7f37
Richard Gooch <rg+github0@safe-mbox.com>
2015-08-29 10:46:59 -0700

    Fix connection leak in Dominator if ImageServer.GetImage() RPC call fails.

--- dom/herd/image.go
+++ dom/herd/image.go
@@ -22,6 +22,7 @@ func (herd *Herd) getImage(name string) *image.Image {
 		fmt.Printf("Error dialing\t%s\n", err)
 		return nil
 	}
+	defer connection.Close()
 	var request imageserver.GetImageRequest
 	request.ImageName = name
 	var reply imageserver.GetImageResponse
@@ -30,7 +31,6 @@ func (herd *Herd) getImage(name string) *image.Image {
 		fmt.Printf("Error calling\t%s\n", err)
 		return nil
 	}
-	connection.Close()
 	// TODO(rgooch): Delete debugging output.
 	if reply.Image != nil {
 		fmt.Printf("Got image: %s\n", name)
