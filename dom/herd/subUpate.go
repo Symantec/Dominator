6f1b21893d59c58c0c54f614690c67cb18900715
Richard Gooch <rg+github0@safe-mbox.com>
2015-09-04 09:24:48 -0700

    Incremental work on buildUpdateRequest().

--- dom/herd/subUpate.go
+++ dom/herd/subUpate.go
@@ -1,9 +1,22 @@
 package herd
 
 import (
+	"github.com/Symantec/Dominator/lib/filesystem"
+	"github.com/Symantec/Dominator/lib/filter"
 	subproto "github.com/Symantec/Dominator/proto/sub"
 )
 
 func (sub *Sub) buildUpdateRequest(request *subproto.UpdateRequest) {
+	subFS := sub.fileSystem
+	requiredImage := sub.herd.getImage(sub.requiredImage)
+	requiredFS := requiredImage.FileSystem
+	filter := requiredImage.Filter
+	compareDirectories(&subFS.Directory, &requiredFS.Directory, filter)
 	// TODO(rgooch): Implement this.
+}
+
+func compareDirectories(subDirectory, requiredDirectory *filesystem.Directory,
+	filter *filter.Filter) {
+	// First look for entries that should be deleted.
+
 }
