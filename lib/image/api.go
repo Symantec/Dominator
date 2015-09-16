cad23ca47e8f000b1b138b6744c5bbe8d58f3cb4
Richard Gooch <rg+github0@safe-mbox.com>
2015-09-15 22:16:14 -0700

    Add Triggers to image.Image.

--- lib/image/api.go
+++ lib/image/api.go
@@ -3,9 +3,11 @@ package image
 import (
 	"github.com/Symantec/Dominator/lib/filesystem"
 	"github.com/Symantec/Dominator/lib/filter"
+	"github.com/Symantec/Dominator/lib/triggers"
 )
 
 type Image struct {
 	Filter     *filter.Filter
 	FileSystem *filesystem.FileSystem
+	Triggers   *triggers.Triggers
 }
