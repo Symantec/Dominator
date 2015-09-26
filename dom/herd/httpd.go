Symantec/Dominator

commit 08c68b5a85bc9ab42391c6b3bb65942d4c9dce6b

Show number and list of deviant subs.

dom/herd/httpd.go
@@ -16,7 +16,8 @@ func (herd *Herd) startServer(portNum uint, daemon bool) error {
 	httpdHerd = herd
 	http.HandleFunc("/", statusHandler)
 	http.HandleFunc("/listSubs", listSubsHandler)
-	http.HandleFunc("/showSubs", showSubsHandler)
+	http.HandleFunc("/showSubs", showAllSubsHandler)
+	http.HandleFunc("/showDeviantSubs", showDeviantSubsHandler)
 	if daemon {
 		go http.Serve(listener, nil)
 	} else {

dom/herd/html.go
@@ -13,14 +13,23 @@ func (herd *Herd) writeHtml(writer io.Writer) {
 		herd.currentScanStartTime.Sub(herd.previousScanStartTime))
 	fmt.Fprintf(writer, "Image server: <a href=\"http://%s/\">%s</a><br>\n",
 		herd.imageServerAddress, herd.imageServerAddress)
-	herd.RLock()
-	numSubs := len(herd.subsByName)
-	herd.RUnlock()
+	numSubs := herd.countSelectedSubs(nil)
 	fmt.Fprintf(writer,
-		"Number of <a href=\"listSubs\">subs</a>: <a href=\"showSubs\">%d</a><br>\n",
+		"Number of <a href=\"listSubs\">subs</a>: <a href=\"showAllSubs\">%d</a><br>\n",
+		numSubs)
+	numSubs = herd.countSelectedSubs(selectDeviantSub)
+	fmt.Fprintf(writer,
+		"Number of deviant subs: <a href=\"showDeviantSubs\">%d</a><br>\n",
 		numSubs)
 	fmt.Fprintf(writer, "Connection slots: %d out of %d<br>\n",
 		len(herd.makeConnectionSemaphore), cap(herd.makeConnectionSemaphore))
 	fmt.Fprintf(writer, "RPC slots: %d out of %d<br>\n",
 		len(herd.pollSemaphore), cap(herd.pollSemaphore))
+}
+
+func selectDeviantSub(sub *Sub) bool {
+	if sub.status == statusUpdating {
+		return true
+	}
+	return false
 }

dom/herd/showSubs.go
@@ -10,10 +10,19 @@ import (
 	"time"
 )
 
-func showSubsHandler(w http.ResponseWriter, req *http.Request) {
+func showAllSubsHandler(w http.ResponseWriter, req *http.Request) {
+	httpdHerd.showSubs(w, "", nil)
+}
+
+func showDeviantSubsHandler(w http.ResponseWriter, req *http.Request) {
+	httpdHerd.showSubs(w, "deviant ", selectDeviantSub)
+}
+
+func (herd *Herd) showSubs(w io.Writer, subType string,
+	selectFunc func(*Sub) bool) {
 	writer := bufio.NewWriter(w)
 	defer writer.Flush()
-	fmt.Fprintln(writer, "<title>Dominator subs</title>")
+	fmt.Fprintf(writer, "<title>Dominator %s subs</title>", subType)
 	fmt.Fprintln(writer, `<style>
                           table, th, td {
                           border-collapse: collapse;
@@ -30,7 +39,7 @@ func showSubsHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(writer, "    <th>Status</b></th>")
 	fmt.Fprintln(writer, "    <th>Staleness</b></th>")
 	fmt.Fprintln(writer, "  </tr>")
-	subs := httpdHerd.getSelectedSubs(nil)
+	subs := herd.getSelectedSubs(selectFunc)
 	for _, sub := range subs {
 		showSub(writer, sub)
 	}

commit 2258533f00d6e8383052a06976b86e627bf1cc11

Fix bug: list of all subs did not work.
Bug introduced in commit 08c68b5a85bc9ab42391c6b3bb65942d4c9dce6b.

dom/herd/httpd.go
@@ -16,7 +16,7 @@ func (herd *Herd) startServer(portNum uint, daemon bool) error {
 	httpdHerd = herd
 	http.HandleFunc("/", statusHandler)
 	http.HandleFunc("/listSubs", listSubsHandler)
-	http.HandleFunc("/showSubs", showAllSubsHandler)
+	http.HandleFunc("/showAllSubs", showAllSubsHandler)
 	http.HandleFunc("/showDeviantSubs", showDeviantSubsHandler)
 	http.HandleFunc("/showCompliantSubs", showCompliantSubsHandler)
 	if daemon {