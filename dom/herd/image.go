cbeb60dec8c1e09ff066973e6dbb1173d52d4501
Richard Gooch <rg+github0@safe-mbox.com>
2015-11-07 13:40:26 -0800

    Fix nil pointer dereference when image not available.

--- dom/herd/image.go
+++ dom/herd/image.go
@@ -44,11 +44,11 @@ func (herd *Herd) getImageHaveLock(name string) *image.Image {
 	}
 	if reply.Image != nil {
 		herd.logger.Printf("Got image: %s\n", name)
+		reply.Image.FileSystem.RebuildInodePointers()
+		reply.Image.FileSystem.BuildEntryMap()
+		reply.Image.FileSystem.BuildInodeToFilenamesTable()
+		reply.Image.FileSystem.BuildHashToInodesTable()
+		herd.imagesByName[name] = reply.Image
 	}
-	reply.Image.FileSystem.RebuildInodePointers()
-	reply.Image.FileSystem.BuildEntryMap()
-	reply.Image.FileSystem.BuildInodeToFilenamesTable()
-	reply.Image.FileSystem.BuildHashToInodesTable()
-	herd.imagesByName[name] = reply.Image
 	return reply.Image
 }
