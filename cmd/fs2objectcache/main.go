e1b3524e1b4084e8a8aadf3ab1f01779842d820f
Richard Gooch <rg+github0@safe-mbox.com>
2015-12-23 21:07:51 -0800

    Add help information to fs2objectcache.

--- cmd/fs2objectcache/main.go
+++ cmd/fs2objectcache/main.go
@@ -12,10 +12,30 @@ var (
 		"Name of root of directory tree to convert to an object cache")
 )
 
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr,
+			"Usage: fs2objectcache [flags...]")
+		fmt.Fprintln(os.Stderr, "Common flags:")
+		flag.PrintDefaults()
+		fmt.Fprintln(os.Stderr)
+		fmt.Fprintln(os.Stderr,
+			"This tool will convert a file-system tree to an object cache.")
+		fmt.Fprintln(os.Stderr,
+			"It is a building block for a secure and fast re-imaging pipeline.")
+		fmt.Fprintln(os.Stderr,
+			"It should only be run from a trusted boot image.")
+	}
+}
+
 func main() {
 	flag.Parse()
 	if *rootDir == "" {
-		fmt.Fprintf(os.Stderr, "rootDir unspecified\n")
+		fmt.Fprintln(os.Stderr, "rootDir unspecified")
+		os.Exit(1)
+	}
+	if *rootDir == "/" {
+		fmt.Fprintln(os.Stderr, "Cannot convert root file-system")
 		os.Exit(1)
 	}
 	subdDirPathname := path.Join(*rootDir, ".subd")
