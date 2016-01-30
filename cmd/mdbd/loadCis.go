192f165fb7e862af0f0ed8f811d099c6e5c07960
Richard Gooch <rg+github0@safe-mbox.com>
2016-01-29 17:47:00 -0800

    Pull image names from instance_metadata in CIS driver for mdbd.

--- cmd/mdbd/loadCis.go
+++ cmd/mdbd/loadCis.go
@@ -10,8 +10,15 @@ import (
 
 func loadCis(reader io.Reader, datacentre string, logger *log.Logger) (
 	*mdb.Mdb, error) {
+
+	type instanceMetadataType struct {
+		RequiredImage string `json:"required_image"`
+		PlannedImage  string `json:"planned_image"`
+	}
+
 	type sourceType struct {
-		Name string
+		Name             string
+		InstanceMetadata instanceMetadataType `json:"instance_metadata"`
 	}
 
 	type hitType struct {
@@ -35,6 +42,12 @@ func loadCis(reader io.Reader, datacentre string, logger *log.Logger) (
 	for _, hit := range inMdb.Hits.Hits {
 		var outMachine mdb.Machine
 		outMachine.Hostname = hit.Source.Name
+		if hit.Source.InstanceMetadata.RequiredImage != "" {
+			outMachine.RequiredImage = hit.Source.InstanceMetadata.RequiredImage
+		}
+		if hit.Source.InstanceMetadata.PlannedImage != "" {
+			outMachine.PlannedImage = hit.Source.InstanceMetadata.PlannedImage
+		}
 		outMdb.Machines = append(outMdb.Machines, outMachine)
 	}
 	return &outMdb, nil
