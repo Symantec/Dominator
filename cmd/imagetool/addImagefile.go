bab1011eff87dce13be3ef6008e83d152a39a9bf
Richard Gooch <rg+github0@safe-mbox.com>
2016-02-15 19:00:14 -0800

    Move some utility functions into addImageCommon.

--- cmd/imagetool/addImagefile.go
+++ cmd/imagetool/addImagefile.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"archive/tar"
-	"bufio"
 	"compress/gzip"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/Symantec/Dominator/imageserver/client"
@@ -15,7 +13,6 @@ import (
 	"github.com/Symantec/Dominator/lib/image"
 	"github.com/Symantec/Dominator/lib/objectclient"
 	"github.com/Symantec/Dominator/lib/srpc"
-	"github.com/Symantec/Dominator/lib/triggers"
 	"github.com/Symantec/Dominator/proto/imageserver"
 	"io"
 	"net/rpc"
@@ -83,63 +80,6 @@ func addImagefile(imageClient *rpc.Client, imageSClient *srpc.Client,
 		return errors.New("remote error: " + err.Error())
 	}
 	return nil
-}
-
-func loadImageFiles(image *image.Image, objectClient *objectclient.ObjectClient,
-	filterFilename, triggersFilename string) error {
-	var err error
-	if filterFilename != "" {
-		image.Filter, err = filter.LoadFilter(filterFilename)
-		if err != nil {
-			return err
-		}
-	}
-	if err := loadTriggers(image, triggersFilename); err != nil {
-		return err
-	}
-	image.BuildLog, err = getAnnotation(objectClient, *buildLog)
-	if err != nil {
-		return err
-	}
-	image.ReleaseNotes, err = getAnnotation(objectClient, *releaseNotes)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func loadTriggers(image *image.Image, triggersFilename string) error {
-	triggersFile, err := os.Open(triggersFilename)
-	if err != nil {
-		return err
-	}
-	defer triggersFile.Close()
-	decoder := json.NewDecoder(triggersFile)
-	var trig triggers.Triggers
-	if err = decoder.Decode(&trig.Triggers); err != nil {
-		return errors.New("error decoding triggers " + err.Error())
-	}
-	image.Triggers = &trig
-	return nil
-}
-
-func getAnnotation(objectClient *objectclient.ObjectClient, name string) (
-	*image.Annotation, error) {
-	if name == "" {
-		return nil, nil
-	}
-	file, err := os.Open(name)
-	if err != nil {
-		return &image.Annotation{URL: name}, nil
-	}
-	defer file.Close()
-	fi, err := file.Stat()
-	if err != nil {
-		return nil, err
-	}
-	reader := bufio.NewReader(file)
-	hash, _, err := objectClient.AddObject(reader, uint64(fi.Size()), nil)
-	return &image.Annotation{Object: &hash}, nil
 }
 
 type dataHandler struct {
