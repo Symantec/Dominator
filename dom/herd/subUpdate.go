5281f893565198fc5b6a4dcf9f11613c2b33df49
Richard Gooch <rg+github0@safe-mbox.com>
2016-02-17 09:15:58 -0800

    Make use FileSystem.FilenameToInodeTable() in buildUpdateRequest().

--- dom/herd/subUpdate.go
+++ dom/herd/subUpdate.go
@@ -16,7 +16,6 @@ type state struct {
 	requiredInodeToSubInode map[uint64]uint64
 	inodesChanged           map[uint64]bool   // Required inode number.
 	inodesCreated           map[uint64]string // Required inode number.
-	subFilenameToInode      map[string]uint64
 	subObjectCacheUsage     map[hash.Hash]uint64
 }
 
@@ -223,7 +222,7 @@ func addInode(request *subproto.UpdateRequest, state *state,
 		var sameDataInode filesystem.GenericInode
 		var sameDataName string
 		for _, name := range names {
-			if inum, found := state.getSubInodeFromFilename(name); found {
+			if inum, found := state.subFS.FilenameToInodeTable()[name]; found {
 				subInode := state.subFS.InodeTable[inum]
 				_, sameMetadata, sameData := filesystem.CompareInodes(
 					subInode, requiredInode, nil)
@@ -268,17 +267,4 @@ func addInode(request *subproto.UpdateRequest, state *state,
 	inode.GenericInode = requiredEntry.Inode()
 	request.InodesToMake = append(request.InodesToMake, inode)
 	state.inodesCreated[requiredEntry.InodeNumber] = myPathName
-}
-
-func (state *state) getSubInodeFromFilename(name string) (uint64, bool) {
-	if state.subFilenameToInode == nil {
-		state.subFilenameToInode = make(map[string]uint64)
-		for inum, names := range state.subFS.InodeToFilenamesTable() {
-			for _, n := range names {
-				state.subFilenameToInode[n] = inum
-			}
-		}
-	}
-	inum, ok := state.subFilenameToInode[name]
-	return inum, ok
 }
