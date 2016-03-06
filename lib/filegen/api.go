2cee2e81a3c803117c4f6bc20db58cb886cc2893
Richard Gooch <rg+github0@safe-mbox.com>
2016-03-06 12:29:44 -0800

    Create skeleton lib/filegen package.

---
+++ lib/filegen/api.go
@@ -0,0 +1,56 @@
+/*
+	Package filegen manages the generation of computed files.
+
+	Package filegen may be used to implement a computed file server. It
+	registers a FileGenerator server with the lib/srpc package. The application
+	may register multiple file generators.
+
+	A generator for the /etc/mdb.json pathname is automatically registered.
+*/
+package filegen
+
+import (
+	"github.com/Symantec/Dominator/lib/mdb"
+	"io"
+	"log"
+	"time"
+)
+
+// FileGenerator is the interface that wraps the Generate method.
+//
+// Generate computes file data from the provided machine information.
+// The logger may be used to log problems.
+// It returns the data, a time.Time indicating when the data are valid until
+// (a zero time indicates the data are valid forever) and an error.
+type FileGenerator interface {
+	Generate(machine mdb.Machine, logger *log.Logger) (
+		data []byte, validUntil time.Time, err error)
+}
+
+type Manager struct {
+	fileGenerators map[string]FileGenerator
+	logger         *log.Logger
+}
+
+// New creates a new *Manager. Only one should be created per application.
+// The logger will be used to log problems.
+func New(logger *log.Logger) *Manager {
+	return newManager(logger)
+}
+
+// RegisterGeneratorForPath registers a FileGenerator for a specific pathname.
+func (m *Manager) RegisterGeneratorForPath(pathname string, gen FileGenerator) {
+	m.registerGeneratorForPath(pathname, gen)
+}
+
+// RegisterFileForPath registers a source file for a specific pathname. The
+// source file is used as the data source.
+func (m *Manager) RegisterFileForPath(pathname string, sourceFile string) {
+	m.registerFileForPath(pathname, sourceFile)
+}
+
+// WriteHtml will write status information about the Manager to w, with
+// appropriate HTML markups.
+func (m *Manager) WriteHtml(writer io.Writer) {
+	m.writeHtml(writer)
+}
---
+++ lib/filegen/new.go
@@ -0,0 +1,34 @@
+package filegen
+
+import (
+	"bytes"
+	"github.com/Symantec/Dominator/lib/json"
+	"github.com/Symantec/Dominator/lib/mdb"
+	"github.com/Symantec/Dominator/lib/srpc"
+	"log"
+	"time"
+)
+
+type jsonType struct{}
+
+type rpcType struct {
+	manager *Manager
+}
+
+func newManager(logger *log.Logger) *Manager {
+	manager := new(Manager)
+	manager.fileGenerators = make(map[string]FileGenerator)
+	manager.logger = logger
+	manager.registerGeneratorForPath("/etc/mdb.json", jsonType{})
+	srpc.RegisterName("FileGenerator", &rpcType{manager})
+	return manager
+}
+
+func (jsonType) Generate(machine mdb.Machine, logger *log.Logger) (
+	[]byte, time.Time, error) {
+	buffer := new(bytes.Buffer)
+	if err := json.WriteWithIndent(buffer, "    ", machine); err != nil {
+		return nil, time.Time{}, err
+	}
+	return buffer.Bytes(), time.Time{}, nil
+}
