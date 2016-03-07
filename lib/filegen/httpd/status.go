e1df9d0aa9ff959a653afc48e3a977a48325d337
Richard Gooch <rg+github0@safe-mbox.com>
2016-03-07 08:50:24 -0800

    Add lib/filegen/httpd package.

---
+++ lib/filegen/httpd/status.go
@@ -0,0 +1,27 @@
+package httpd
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/Symantec/Dominator/lib/html"
+	"net/http"
+)
+
+func (s *state) statusHandler(w http.ResponseWriter, req *http.Request) {
+	writer := bufio.NewWriter(w)
+	defer writer.Flush()
+	fmt.Fprintln(writer, "<title>Basic filegen server status page</title>")
+	fmt.Fprintln(writer, "<body>")
+	fmt.Fprintln(writer, "<center>")
+	fmt.Fprintln(writer, "<h1>Basic filegen server status page</h1>")
+	fmt.Fprintln(writer, "</center>")
+	html.WriteHeaderWithRequest(writer, req)
+	fmt.Fprintln(writer, "<h3>")
+	for _, htmlWriter := range htmlWriters {
+		htmlWriter.WriteHtml(writer)
+	}
+	fmt.Fprintln(writer, "</h3>")
+	fmt.Fprintln(writer, "<hr>")
+	html.WriteFooter(writer)
+	fmt.Fprintln(writer, "</body>")
+}
