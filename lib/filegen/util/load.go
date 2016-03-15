0e2a5d7d98858194a0dbb4bee9d14929b0a1d3b1
Richard Gooch <rg+github0@safe-mbox.com>
2016-03-14 18:22:43 -0700

    Add basic parser to lib/filegen/util.LoadConfiguration().

--- lib/filegen/util/load.go
+++ lib/filegen/util/load.go
@@ -10,10 +10,12 @@ import (
 type configFunc func(*filegen.Manager, []string) error
 
 type configType struct {
-	minFields  int
-	maxFields  int
+	minParams  int
+	maxParams  int
 	configFunc configFunc
 }
+
+var configs = map[string]configType{}
 
 func loadConfiguration(manager *filegen.Manager, filename string) error {
 	if filename == "" {
@@ -25,7 +27,21 @@ func loadConfiguration(manager *filegen.Manager, filename string) error {
 	}
 	for _, line := range lines {
 		fields := strings.Fields(line)
-		_ = fields // TODO(rgooch): Implement.
+		if len(fields) < 2 {
+			continue
+		}
+		config, ok := configs[fields[0]]
+		if !ok {
+			continue
+		}
+		numParams := len(fields) - 1
+		if numParams < config.minParams {
+			return fmt.Errorf("insufficient params in line: %s", line)
+		}
+		if config.maxParams >= 0 && numParams > config.maxParams {
+			return fmt.Errorf("too many params in line: %s", line)
+		}
+		// TODO(rgooch): Implement.
 	}
 	return nil
 }
