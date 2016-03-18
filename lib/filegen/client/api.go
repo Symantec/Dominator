50f348b7835b02ba670d12095be7800c403098d4
Richard Gooch <rg+github0@safe-mbox.com>
2016-03-18 00:33:19 -0700

    Improved documentation for lib/filegen/client package.

--- lib/filegen/client/api.go
+++ lib/filegen/client/api.go
@@ -47,14 +47,18 @@ type Manager struct {
 	logger                 *log.Logger
 }
 
-// New creates a new *Manager. Only one should be created per application.
+// New creates a new *Manager. Object data will be added to the object server
+// objSrv. Only one Manager should be created per application.
 // The logger will be used to log problems.
 func New(objSrv objectserver.ObjectServer, logger *log.Logger) *Manager {
 	return newManager(objSrv, logger)
 }
 
 // Add will add a machine to the Manager. Re-adding a machine will result in a
-// panic. The length of the channel buffer is given by size.
+// panic. The length of the returned channel buffer is determined by size.
+// A channel is returned from which file information may be read. It is
+// guaranteed that corresponding object data is in the object server before
+// file information is available.
 func (m *Manager) Add(machine Machine, size uint) <-chan []proto.FileInfo {
 	updateChannel := make(chan []proto.FileInfo, size)
 	mach := buildMachine(machine)
@@ -63,12 +67,14 @@ func (m *Manager) Add(machine Machine, size uint) <-chan []proto.FileInfo {
 	return updateChannel
 }
 
-// Remove will remove a machine from the Manager.
+// Remove will remove a machine from the Manager. The corresponding file info
+// channel will be closed.
 func (m *Manager) Remove(hostname string) {
 	m.removeMachineChannel <- hostname
 }
 
-// Update will update the machine data for a machine.
+// Update will update the machine data for a machine, which may result in file
+// info data being sent to the corresponding channel.
 func (m *Manager) Update(machine Machine) {
 	m.updateMachineChannel <- buildMachine(machine)
 }
