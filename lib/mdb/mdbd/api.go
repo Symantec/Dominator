f5bf74361f81fab20c8a59e25f4e7038a05380f1
Richard Gooch <rg+github0@safe-mbox.com>
2016-03-25 06:38:10 -0700

    Add documentation to lib/mdb/mdbd package.

--- lib/mdb/mdbd/api.go
+++ lib/mdb/mdbd/api.go
@@ -1,3 +1,8 @@
+/*
+	Package mdbd implements a simple MDB watcher.
+
+	Package mdbd may be used to read MDB data from a file and watch for updates.
+*/
 package mdbd
 
 import (
@@ -5,6 +10,13 @@ import (
 	"log"
 )
 
+// StartMdbDaemon starts an in-process "daemon" goroutine which watches the
+// file named by mdbFileName for MDB updates. The default format is JSON, but
+// if the filename extension is ".gob" then GOB format is read.
+// If the file is replaced by a different inode, MDB data are read from the new
+// inode and if the MDB data are different than previously read, they are sent
+// over the returned channel.
+// The logger will be used to log problems.
 func StartMdbDaemon(mdbFileName string, logger *log.Logger) <-chan *mdb.Mdb {
 	return startMdbDaemon(mdbFileName, logger)
 }
