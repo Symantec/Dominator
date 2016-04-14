a4890a1c8c8a3c0f12b0e35791cd02e7bc410886
Richard Gooch <rg+github0@safe-mbox.com>
2016-04-14 09:39:50 -0700

    Add tests for lib/verstr.Less().

---
+++ lib/verstr/less_test.go
@@ -0,0 +1,22 @@
+package verstr
+
+import (
+	"testing"
+)
+
+func TestLess(t *testing.T) {
+	var tests = []struct {
+		left, right string
+		want        bool
+	}{
+		{"file.1.ext", "file.10.ext", true},
+		{"file.10.ext", "file.1.ext", false},
+		{"sparse", "sparse.0", true},
+		{"sparse.0", "sparse", false},
+	}
+	for _, test := range tests {
+		if got := Less(test.left, test.right); got != test.want {
+			t.Errorf("Less(%q, %q) = %v", test.left, test.right, got)
+		}
+	}
+}
