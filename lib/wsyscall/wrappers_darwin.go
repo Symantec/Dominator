66915d964806ae68194729ba7b7b029df226ead5
Richard Gooch <rg+github0@safe-mbox.com>
2016-04-22 08:42:49 -0700

    Check in initial lib/wsyscall package.

---
+++ lib/wsyscall/wrappers_darwin.go
@@ -0,0 +1,19 @@
+package wsyscall
+
+import "syscall"
+
+func convertStat(dest *Stat_t, source *syscall.Stat_t) {
+	dest.Dev = uint64(source.Dev)
+	dest.Ino = source.Ino
+	dest.Nlink = uint64(source.Nlink)
+	dest.Mode = uint32(source.Mode)
+	dest.Uid = source.Uid
+	dest.Gid = source.Gid
+	dest.Rdev = uint64(source.Rdev)
+	dest.Size = source.Size
+	dest.Blksize = int64(source.Blksize)
+	dest.Blocks = source.Blocks
+	dest.Atim = source.Atimespec
+	dest.Mtim = source.Mtimespec
+	dest.Ctim = source.Ctimespec
+}
