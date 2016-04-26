2abcc7e93610468f71613594989cc07d91cc2c02
Richard Gooch <rg+github0@safe-mbox.com>
2016-04-25 21:52:41 -0700

    Improve lib/x509util documentation.

--- lib/x509util/api.go
+++ lib/x509util/api.go
@@ -1,8 +1,14 @@
+/*
+	Package x509util provides utility functions to process X509 certificates.
+*/
 package x509util
 
 import "crypto/x509"
 
 // GetPermittedMethods decodes the list of permitted methods in the certificate.
+// The permitted methods are returned as keys in a map. An empty map indicates
+// no methods are permitted. If there is a problem parsing the information an
+// error is returned.
 func GetPermittedMethods(cert *x509.Certificate) (map[string]struct{}, error) {
 	return getPermittedMethods(cert)
 }
