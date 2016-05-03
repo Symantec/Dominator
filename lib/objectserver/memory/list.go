afd9e5401948c31b1bd7fcdb02d54f4d3b72f494
Richard Gooch <rg+github0@safe-mbox.com>
2016-05-02 22:46:51 -0700

    Add lib/objectserver/memory.ListObjectSizes() method.

--- lib/objectserver/memory/list.go
+++ lib/objectserver/memory/list.go
@@ -4,6 +4,16 @@ import (
 	"github.com/Symantec/Dominator/lib/hash"
 )
 
+func (objSrv *ObjectServer) listObjectSizes() map[hash.Hash]uint64 {
+	objSrv.rwLock.RLock()
+	defer objSrv.rwLock.RUnlock()
+	sizesMap := make(map[hash.Hash]uint64, len(objSrv.objectMap))
+	for hashVal, data := range objSrv.objectMap {
+		sizesMap[hashVal] = uint64(len(data))
+	}
+	return sizesMap
+}
+
 func (objSrv *ObjectServer) listObjects() []hash.Hash {
 	objSrv.rwLock.RLock()
 	defer objSrv.rwLock.RUnlock()
