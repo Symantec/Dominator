34c67ac3869d8fcdfc83a0f9ff1f21d342fcd7d7
Richard Gooch <rg+github0@safe-mbox.com>
2016-05-04 22:38:53 -0700

    Add ObjectServer.CheckObjects SRPC method.

--- objectserver/rpcd/checkObjects.go
+++ objectserver/rpcd/checkObjects.go
@@ -1,8 +1,30 @@
 package rpcd
 
 import (
+	"encoding/gob"
+	"github.com/Symantec/Dominator/lib/srpc"
 	"github.com/Symantec/Dominator/proto/objectserver"
 )
+
+func (t *srpcType) CheckObjects(conn *srpc.Conn) error {
+	var request objectserver.CheckObjectsRequest
+	var response objectserver.CheckObjectsResponse
+	decoder := gob.NewDecoder(conn)
+	if err := decoder.Decode(&request); err != nil {
+		_, err = conn.WriteString(err.Error() + "\n")
+		return err
+	}
+	var err error
+	response.ObjectSizes, err = t.objectServer.CheckObjects(request.Hashes)
+	if err != nil {
+		_, err = conn.WriteString(err.Error() + "\n")
+		return err
+	}
+	if _, err := conn.WriteString("\n"); err != nil {
+		return err
+	}
+	return gob.NewEncoder(conn).Encode(response)
+}
 
 func (t *rpcType) CheckObjects(
 	request objectserver.CheckObjectsRequest,
