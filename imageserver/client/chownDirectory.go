9ed1bfb8ff1b1daa5e9baff7a3082cca5a046b67
Richard Gooch <rg+github0@safe-mbox.com>
2016-05-17 16:37:50 -0700

    Easy changes to imageserver/client functions to new RequestReply method.

--- imageserver/client/chownDirectory.go
+++ imageserver/client/chownDirectory.go
@@ -1,35 +1,13 @@
 package client
 
 import (
-	"encoding/gob"
-	"errors"
 	"github.com/Symantec/Dominator/lib/srpc"
 	"github.com/Symantec/Dominator/proto/imageserver"
 )
 
 func callChownDirectory(client *srpc.Client, dirname, ownerGroup string) error {
-	conn, err := client.Call("ImageServer.ChownDirectory")
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-	encoder := gob.NewEncoder(conn)
 	request := imageserver.ChangeOwnerRequest{DirectoryName: dirname,
 		OwnerGroup: ownerGroup}
 	var reply imageserver.ChangeOwnerResponse
-	if err := encoder.Encode(request); err != nil {
-		return err
-	}
-	conn.Flush()
-	str, err := conn.ReadString('\n')
-	if err != nil {
-		return err
-	}
-	if str != "\n" {
-		return errors.New(str[:len(str)-1])
-	}
-	if err := gob.NewDecoder(conn).Decode(&reply); err != nil {
-		return err
-	}
-	return nil
+	return client.RequestReply("ImageServer.ChownDirectory", request, &reply)
 }
--- imageserver/client/makeDirectory.go
+++ imageserver/client/makeDirectory.go
@@ -1,31 +1,12 @@
 package client
 
 import (
-	"encoding/gob"
-	"errors"
 	"github.com/Symantec/Dominator/lib/srpc"
 	"github.com/Symantec/Dominator/proto/imageserver"
 )
 
 func callMakeDirectory(client *srpc.Client, dirname string) error {
-	conn, err := client.Call("ImageServer.MakeDirectory")
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-	encoder := gob.NewEncoder(conn)
 	request := imageserver.MakeDirectoryRequest{dirname}
 	var reply imageserver.MakeDirectoryResponse
-	if err := encoder.Encode(request); err != nil {
-		return err
-	}
-	conn.Flush()
-	str, err := conn.ReadString('\n')
-	if err != nil {
-		return err
-	}
-	if str != "\n" {
-		return errors.New(str[:len(str)-1])
-	}
-	return gob.NewDecoder(conn).Decode(&reply)
+	return client.RequestReply("ImageServer.MakeDirectory", request, &reply)
 }
