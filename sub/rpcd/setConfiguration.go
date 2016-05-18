a5b29b491158ba10053dc1262453fdb6913aac61
Richard Gooch <rg+github0@safe-mbox.com>
2016-05-18 07:27:40 -0700

    Switch subd SRPC methods to request/reply style methods.

--- sub/rpcd/setConfiguration.go
+++ sub/rpcd/setConfiguration.go
@@ -1,32 +1,13 @@
 package rpcd
 
 import (
-	"encoding/gob"
 	"github.com/Symantec/Dominator/lib/filter"
 	"github.com/Symantec/Dominator/lib/srpc"
 	"github.com/Symantec/Dominator/proto/sub"
 )
 
-func (t *rpcType) SetConfiguration(conn *srpc.Conn) error {
-	defer conn.Flush()
-	var request sub.SetConfigurationRequest
-	var response sub.SetConfigurationResponse
-	decoder := gob.NewDecoder(conn)
-	if err := decoder.Decode(&request); err != nil {
-		_, err = conn.WriteString(err.Error() + "\n")
-		return err
-	}
-	if err := t.setConfiguration(request, &response); err != nil {
-		_, err = conn.WriteString(err.Error() + "\n")
-		return err
-	}
-	if _, err := conn.WriteString("\n"); err != nil {
-		return err
-	}
-	return gob.NewEncoder(conn).Encode(response)
-}
-
-func (t *rpcType) setConfiguration(request sub.SetConfigurationRequest,
+func (t *rpcType) SetConfiguration(conn *srpc.Conn,
+	request sub.SetConfigurationRequest,
 	reply *sub.SetConfigurationResponse) error {
 	t.scannerConfiguration.FsScanContext.GetContext().SetSpeedPercent(
 		request.ScanSpeedPercent)
