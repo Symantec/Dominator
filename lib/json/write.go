d185f36c38ec1729e0e917d0c4b64646dd074ee1
Richard Gooch <rg+github0@safe-mbox.com>
2016-06-23 21:59:48 -0700

    Use new encoding/json.Encoder.SetIndent() function. Requires go1.7beta1.

--- lib/json/write.go
+++ lib/json/write.go
@@ -1,20 +1,12 @@
 package json
 
 import (
-	"bytes"
 	"encoding/json"
 	"io"
 )
 
 func writeWithIndent(w io.Writer, indent string, value interface{}) error {
-	b, err := json.Marshal(value)
-	if err != nil {
-		return err
-	}
-	var out bytes.Buffer
-	if err := json.Indent(&out, b, "", indent); err != nil {
-		return err
-	}
-	_, err = out.WriteTo(w)
-	return err
+	encoder := json.NewEncoder(w)
+	encoder.SetIndent("", indent)
+	return encoder.Encode(value)
 }
