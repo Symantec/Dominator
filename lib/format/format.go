e6ce0f65c3e0605e5e282eadf08a60c33998d263
Richard Gooch <rg+github0@safe-mbox.com>
2016-07-10 22:44:26 -0700

    Sort functions in lib/format package.

--- lib/format/format.go
+++ lib/format/format.go
@@ -8,20 +8,6 @@ import (
 	"time"
 )
 
-// FormatBytes returns a string with the number of bytes specified converted
-// into a human-friendly format with a binary multiplier (i.e. GiB).
-func FormatBytes(bytes uint64) string {
-	if bytes>>30 > 100 {
-		return fmt.Sprintf("%d GiB", bytes>>30)
-	} else if bytes>>20 > 100 {
-		return fmt.Sprintf("%d MiB", bytes>>20)
-	} else if bytes>>10 > 100 {
-		return fmt.Sprintf("%d KiB", bytes>>10)
-	} else {
-		return fmt.Sprintf("%d B", bytes)
-	}
-}
-
 // Duration is similar to the time.Duration.String method from the standard
 // library but is more readable and shows only 3 digits of precision when
 // duration is less than 1 minute.
@@ -43,5 +29,19 @@ func Duration(duration time.Duration) string {
 		days := duration / day
 		duration %= day
 		return fmt.Sprintf("%dd%s", days, duration)
+	}
+}
+
+// FormatBytes returns a string with the number of bytes specified converted
+// into a human-friendly format with a binary multiplier (i.e. GiB).
+func FormatBytes(bytes uint64) string {
+	if bytes>>30 > 100 {
+		return fmt.Sprintf("%d GiB", bytes>>30)
+	} else if bytes>>20 > 100 {
+		return fmt.Sprintf("%d MiB", bytes>>20)
+	} else if bytes>>10 > 100 {
+		return fmt.Sprintf("%d KiB", bytes>>10)
+	} else {
+		return fmt.Sprintf("%d B", bytes)
 	}
 }
