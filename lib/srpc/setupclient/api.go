7fc89143588b6f97d240e97c55208e5243e3766e
Richard Gooch <rg+github0@safe-mbox.com>
2016-07-16 12:39:43 -0700

    Switch clients from -cert{File,Key} to -certDirectory option.

--- lib/srpc/setupclient/api.go
+++ lib/srpc/setupclient/api.go
@@ -6,11 +6,10 @@
 */
 package setupclient
 
-// SetupTls loads a client certificate from files and registers it with the
-// lib/srpc package. The following command-line flags are registered
+// SetupTls loads zero or more client certificates from files and registers them
+// with the lib/srpc package. The following command-line flags are registered
 // with the standard flag package:
-//   -certFile: Name of file containing the SSL certificate
-//   -keyFile:  Name of file containing the SSL key
+//   -certDirectory: Name of directory containing user SSL certificates
 func SetupTls(ignoreMissingCerts bool) error {
 	return setupTls(ignoreMissingCerts)
 }
