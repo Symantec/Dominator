b417f267ac560105d28f4ec324c3daf474b84eb9
Richard Gooch <rg+github0@safe-mbox.com>
2016-08-21 22:20:45 -0700

    Check in dom/images package.

---
+++ dom/images/api.go
@@ -0,0 +1,50 @@
+package images
+
+import (
+	"github.com/Symantec/Dominator/lib/image"
+	"log"
+	"sync"
+)
+
+type Manager struct {
+	imageServerAddress string
+	logger             *log.Logger
+	loggedDialFailure  bool
+	sync.RWMutex
+	// Protected by lock.
+	imageInterestChannel chan<- map[string]struct{}
+	imageRequestChannel  chan<- string
+	imageExpireChannel   chan<- string
+	imagesByName         map[string]*image.Image
+	missingImages        map[string]error
+}
+
+func New(imageServerAddress string, logger *log.Logger) *Manager {
+	return newManager(imageServerAddress, logger)
+}
+
+func (m *Manager) Get(name string, wait bool) (*image.Image, error) {
+	if name == "" {
+		return nil, nil
+	}
+	if wait {
+		return m.getWait(name)
+	}
+	return m.getNoWait(name)
+}
+
+func (m *Manager) GetNoError(name string) *image.Image {
+	if name == "" {
+		return nil
+	}
+	img, _ := m.getNoWait(name)
+	return img
+}
+
+func (m *Manager) SetImageInterestList(images map[string]struct{}, wait bool) {
+	m.setImageInterestList(images, wait)
+}
+
+func (m *Manager) String() string {
+	return m.imageServerAddress
+}
