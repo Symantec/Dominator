8f706d9dbbb82321e51d4c16df190af7d8a9f2b8
Travis Keep <travis_keep@symantec.com>
2016-11-16 10:35:26 -0800

    Richard's comments.

--- lib/llog/api.go
+++ lib/llog/api.go
@@ -4,24 +4,6 @@ import (
 	"io"
 )
 
-// Level indicates logging level. The higher the value, the less important.
-// Any non-negative integer is always less important than a named constant.
-type Level int
-
-const (
-	// Least important
-	Info Level = -1 - iota
-	Warn
-	// Most important
-	Err
-)
-
-// String returns the string representation of this level in all caps.
-func (l *Level) String() string {
-	// TODO
-	return ""
-}
-
 // Logger instances log messages to a particular writer. A Logger instance is
 // safe to use with multiple goroutines. Moreover, multiple instances logging
 // to the same writer synchronise their writes.
@@ -29,20 +11,26 @@ type Logger struct {
 	// TODO
 }
 
-// New creates a new logger, using --logLevel command-line flag to
-// decide what messages get logged. If the --logLevel
-// command-line flag is not present, NewDefault uses importance of Info.
-// --logLevel command-line flag is of form level or subsystem:level. For
-// example --logLevel Warn or --logLevel images:Info or --logLevel images:2.
-// --logLevel may be used more than once on a command line. Log levels for
-// subsystems e.g --logLevel images:Info always override global log level e.g
-// --logLevel 2. In the presence of conflict such as --logLevel images:Info
-// --logLevel images:Err, the last -logLevel, in this case images:Err, wins.
+// New creates a new logger. New uses --logLevel command-line flag to
+// decide what debug messages to log. If the --logLevel command-line flag
+// is not present, the returned logger will not log any debug messages.
+// The Returned logger will always log the other types of messages:
+// Fatal, Err, Warn, Info
+//
+// --logLevel=2 means log debug messages with levels 0, 1, and 2. --logLevel=0
+// means log debug messages with level 0 only. --logLevel=images:1 means
+// log debug messages with levels 0 and 1 for the "images" subsystem only.
+// --logLevel may be used more than once on a command line to specify
+// different log levels for different subsystems.
+// For example --logLevel=0 --logLevel=images:2 --logLevel=files:3 logs
+// debug messages up to level 3 for the files subsystem, up to level 2 for
+// the images subsystem and level 0 only for all the other subsystems.
 //
 // writer is where the returned logger writes its messages. If the writer
 // implementation has a no-arg Flush() method that returns an error,
 // the returned logger calls it after logging a fatal or panic message
-// just before terminating the process.
+// just before terminating the process. subSystemName is the name of the
+// subsystem.
 func New(writer io.Writer, subSystemName string) *Logger {
 	// TODO
 	return nil
@@ -138,22 +126,24 @@ func (l *Logger) Infoln(v ...interface{}) {
 }
 
 // TODO
-func (l *Logger) Log(level Level, v ...interface{}) {
+func (l *Logger) Debug(level uint8, v ...interface{}) {
 	// TODO
 }
 
 // TODO
-func (l *Logger) Logf(level Level, format string, v ...interface{}) {
+func (l *Logger) Debugf(level uint8, format string, v ...interface{}) {
 	// TODO
 }
 
 // TODO
-func (l *Logger) Logln(level Level, v ...interface{}) {
+func (l *Logger) Debugln(level uint8, v ...interface{}) {
 	// TODO
 }
 
-// Level returns the level of this log. That is, which messages it logs.
-func (l *Logger) Level() Level {
+// SetLevel sets the debug level of this instance. After this call, this
+// instance will log debug messages with level up to and including
+// maxLevel. A negative maxLevel means that debug logging is turned off.
+// A maxLevel >= 255 means all debug messages get logged.
+func (l *Logger) SetLevel(maxLevel int) {
 	// TODO
-	return Info
 }
