9f2418f70cd6099685415ef9ced70a9462be0ccf
Travis Keep <travis_keep@symantec.com>
2016-11-21 10:06:37 -0800

    Git rid of Fatal,Err,Warn,Info on account of logging article.
    Add LoggerInterfac.

--- lib/llog/api.go
+++ lib/llog/api.go
@@ -1,12 +1,16 @@
 package llog
 
-import (
-	"io"
-)
+// LoggerInterface is a simple interface for logging messages.
+// LoggerInterface instances must be safe to use with multiple goroutines.
+// Note that log.Logger already implements this interface.
+type LoggerInterface interface {
+	// calldepth is the call depth, usually 2. s is the message to log
+	// without the timestamps.
+	Output(calldepth int, s string) error
+}
 
-// Logger instances log messages to a particular writer. A Logger instance is
-// safe to use with multiple goroutines. Moreover, multiple instances logging
-// to the same writer synchronise their writes.
+// Logger instances log messages to a particular LoggerInterface. A Logger
+// instance is safe to use with multiple goroutines.
 type Logger struct {
 	// TODO
 }
@@ -14,8 +18,6 @@ type Logger struct {
 // New creates a new logger. New uses --logLevel command-line flag to
 // decide what debug messages to log. If the --logLevel command-line flag
 // is not present, the returned logger will not log any debug messages.
-// The Returned logger will always log the other types of messages:
-// Fatal, Err, Warn, Info
 //
 // --logLevel=2 means log debug messages with levels 0, 1, and 2. --logLevel=0
 // means log debug messages with level 0 only. --logLevel=images:1 means
@@ -26,43 +28,10 @@ type Logger struct {
 // debug messages up to level 3 for the files subsystem, up to level 2 for
 // the images subsystem and level 0 only for all the other subsystems.
 //
-// writer is where the returned logger writes its messages. If the writer
-// implementation has a no-arg Flush() method that returns an error,
-// the returned logger calls it after logging a fatal or panic message
-// just before terminating the process. subSystemName is the name of the
-// subsystem.
-func New(writer io.Writer, subSystemName string) *Logger {
+// writer is where the returned logger writes its messages.
+func New(writer LoggerInterface, subSystemName string) *Logger {
 	// TODO
 	return nil
-}
-
-// Fatal logs an Err message, flushes the writer, and terminates the process.
-func (l *Logger) Fatal(v ...interface{}) {
-	// TODO
-}
-
-// TODO
-func (l *Logger) Fatalf(format string, v ...interface{}) {
-	// TODO
-}
-
-func (l *Logger) Fatalln(v ...interface{}) {
-	// TODO
-}
-
-// Panic logs an Err message, flushes the writer, and panics.
-func (l *Logger) Panic(v ...interface{}) {
-	// TODO
-}
-
-// TODO
-func (l *Logger) Panicf(format string, v ...interface{}) {
-	// TODO
-}
-
-// TODO
-func (l *Logger) Panicln(v ...interface{}) {
-	// TODO
 }
 
 // Print is equivalent to Log(Info, v...)
@@ -77,51 +46,6 @@ func (l *Logger) Printf(format string, v ...interface{}) {
 
 // TODO
 func (l *Logger) Println(v ...interface{}) {
-	// TODO
-}
-
-// TODO
-func (l *Logger) Err(v ...interface{}) {
-	// TODO
-}
-
-// TODO
-func (l *Logger) Errf(format string, v ...interface{}) {
-	// TODO
-}
-
-// TODO
-func (l *Logger) Errln(v ...interface{}) {
-	// TODO
-}
-
-// TODO
-func (l *Logger) Warn(v ...interface{}) {
-	// TODO
-}
-
-// TODO
-func (l *Logger) Warnf(format string, v ...interface{}) {
-	// TODO
-}
-
-// TODO
-func (l *Logger) Warnln(v ...interface{}) {
-	// TODO
-}
-
-// TODO
-func (l *Logger) Info(v ...interface{}) {
-	// TODO
-}
-
-// TODO
-func (l *Logger) Infof(format string, v ...interface{}) {
-	// TODO
-}
-
-// TODO
-func (l *Logger) Infoln(v ...interface{}) {
 	// TODO
 }
 
