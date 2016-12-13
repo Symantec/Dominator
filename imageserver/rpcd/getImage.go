f0ea8147f57c810e8bfe32d3a7e103d1e333d8bb
Richard Gooch <rg+github0@safe-mbox.com>
2016-12-13 07:37:18 -0800

    Add timeout support to ImageServer.GetImage SRPC method.

--- imageserver/rpcd/getImage.go
+++ imageserver/rpcd/getImage.go
@@ -3,6 +3,7 @@ package rpcd
 import (
 	"github.com/Symantec/Dominator/lib/srpc"
 	"github.com/Symantec/Dominator/proto/imageserver"
+	"time"
 )
 
 func (t *srpcType) GetImage(conn *srpc.Conn,
@@ -11,5 +12,32 @@ func (t *srpcType) GetImage(conn *srpc.Conn,
 	var response imageserver.GetImageResponse
 	response.Image = t.imageDataBase.GetImage(request.ImageName)
 	*reply = response
-	return nil
+	if response.Image != nil || request.Timeout == 0 {
+		return nil
+	}
+	// Image not found yet and willing to wait.
+	addCh := t.imageDataBase.RegisterAddNotifier()
+	defer func() {
+		t.imageDataBase.UnregisterAddNotifier(addCh)
+		select {
+		case <-addCh:
+		default:
+		}
+	}()
+	timer := time.NewTimer(request.Timeout)
+	for {
+		select {
+		case imageName := <-addCh:
+			if imageName == request.ImageName {
+				if !timer.Stop() {
+					<-timer.C
+				}
+				response.Image = t.imageDataBase.GetImage(request.ImageName)
+				*reply = response
+				return nil
+			}
+		case <-timer.C:
+			return nil
+		}
+	}
 }
