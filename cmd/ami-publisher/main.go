659797fb2352e4bdab3a5a18eb2710045cee4a3f
Richard Gooch <rg+github0@safe-mbox.com>
2016-12-23 18:49:55 -0800

    Check in ami-publisher command.

---
+++ cmd/ami-publisher/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/Symantec/Dominator/lib/constants"
+	"github.com/Symantec/Dominator/lib/flagutil"
+	"github.com/Symantec/Dominator/lib/srpc/setupclient"
+	"log"
+	"os"
+	"time"
+)
+
+var (
+	amiName   = flag.String("amiName", "", "AMI Name property")
+	expiresIn = flag.Duration("expiresIn", time.Hour,
+		"Date to set for the ExpiresAt tag")
+	imageServerHostname = flag.String("imageServerHostname", "localhost",
+		"Hostname of imageserver")
+	imageServerPortNum = flag.Uint("imageServerPortNum",
+		constants.ImageServerPortNumber, "Port number of imageserver")
+	minFreeBytes = flag.Uint64("minFreeBytes", 1<<28,
+		"minimum number of free bytes in image")
+	skipFile = flag.String("skipFile", "",
+		"JSON encoded file containing targets to skip")
+	skipString = flag.String("skipString", "",
+		"JSON encoded string containing targets to skip")
+	tagsFile = flag.String("tagsFile", "",
+		"JSON encoded file containing tags to apply to AMIs")
+	targetAccounts flagutil.StringList
+	targetRegions  flagutil.StringList
+)
+
+func init() {
+	flag.Var(&targetAccounts, "targetAccounts",
+		"List of target account profile names")
+	flag.Var(&targetRegions, "targetRegions",
+		"List of target regions (default all regions)")
+}
+
+func printUsage() {
+	fmt.Fprintln(os.Stderr,
+		"Usage: ami-publisher [flags...] publish [args...]")
+	fmt.Fprintln(os.Stderr, "Common flags:")
+	flag.PrintDefaults()
+	fmt.Fprintln(os.Stderr, "Commands:")
+	fmt.Fprintln(os.Stderr, "  publish stream-name image-leaf-name")
+}
+
+type commandFunc func([]string, *log.Logger)
+
+type subcommand struct {
+	command string
+	minArgs int
+	maxArgs int
+	cmdFunc commandFunc
+}
+
+var subcommands = []subcommand{
+	{"publish", 2, 2, publishSubcommand},
+}
+
+func main() {
+	flag.Usage = printUsage
+	flag.Parse()
+	if flag.NArg() < 1 {
+		printUsage()
+		os.Exit(2)
+	}
+	logger := log.New(os.Stderr, "", log.LstdFlags)
+	if err := setupclient.SetupTls(true); err != nil {
+		logger.Println(err)
+		os.Exit(1)
+	}
+	numSubcommandArgs := flag.NArg() - 1
+	for _, subcommand := range subcommands {
+		if flag.Arg(0) == subcommand.command {
+			if numSubcommandArgs < subcommand.minArgs ||
+				(subcommand.maxArgs >= 0 &&
+					numSubcommandArgs > subcommand.maxArgs) {
+				printUsage()
+				os.Exit(2)
+			}
+			subcommand.cmdFunc(flag.Args()[1:], logger)
+			os.Exit(3)
+		}
+	}
+	printUsage()
+	os.Exit(2)
+}
---
+++ cmd/ami-publisher/publish.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/Symantec/Dominator/imagepublishers/amipublisher"
+	libjson "github.com/Symantec/Dominator/lib/json"
+	"log"
+	"os"
+	"path"
+	"time"
+)
+
+func publishSubcommand(args []string, logger *log.Logger) {
+	imageServerAddr := fmt.Sprintf("%s:%d",
+		*imageServerHostname, *imageServerPortNum)
+	err := publish(imageServerAddr, args[0], args[1], logger)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error publishing image: %s\n", err)
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func publish(imageServerAddress string, streamName string, imageLeafName string,
+	logger *log.Logger) error {
+	streamName = path.Clean(streamName)
+	imageLeafName = path.Clean(imageLeafName)
+	skipTargets := make([]amipublisher.Target, 0)
+	if *skipFile != "" {
+		var skip []amipublisher.Target
+		if err := libjson.ReadFromFile(*skipFile, &skip); err != nil {
+			return fmt.Errorf("error skipTargets file: %s", err)
+		}
+		skipTargets = append(skipTargets, skip...)
+	}
+	if *skipString != "" {
+		var skip []amipublisher.Target
+		if err := json.Unmarshal([]byte(*skipString), &skip); err != nil {
+			return fmt.Errorf("error decoding skipString file: %s", err)
+		}
+		skipTargets = append(skipTargets, skip...)
+	}
+
+	var tags map[string]string
+	if *tagsFile != "" {
+		if err := libjson.ReadFromFile(*tagsFile, &tags); err != nil {
+			return fmt.Errorf("error loading tags file: %s", err)
+		}
+	}
+	if tags == nil {
+		tags = make(map[string]string)
+	}
+	if *expiresIn > 0 {
+		expirationTime := time.Now().Add(*expiresIn)
+		tags["ExpiresAt"] = expirationTime.UTC().Format("2006-01-02:15:04:05")
+	}
+	results, err := amipublisher.Publish(imageServerAddress, streamName,
+		imageLeafName, *minFreeBytes, *amiName, tags, targetAccounts,
+		targetRegions, skipTargets, logger)
+	if err != nil {
+		return err
+	}
+	if err := libjson.WriteWithIndent(os.Stdout, "    ", results); err != nil {
+		return err
+	}
+	for _, result := range results {
+		if result.Error != nil {
+			os.Exit(2)
+		}
+	}
+	return nil
+}
