199a70f628e17259f37094cd24669bb64f813306
Richard Gooch <rg+github0@safe-mbox.com>
2016-12-30 07:19:48 -0800

    Use new imageunpacker/client package in unpacker-tool.

--- cmd/unpacker-tool/prepareForCapture.go
+++ cmd/unpacker-tool/prepareForCapture.go
@@ -2,22 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/Symantec/Dominator/imageunpacker/client"
 	"github.com/Symantec/Dominator/lib/srpc"
-	proto "github.com/Symantec/Dominator/proto/imageunpacker"
 	"os"
 )
 
-func prepareForCaptureSubcommand(client *srpc.Client, args []string) {
-	if err := prepareForCapture(client, args[0]); err != nil {
+func prepareForCaptureSubcommand(srpcClient *srpc.Client, args []string) {
+	if err := client.PrepareForCapture(srpcClient, args[0]); err != nil {
 		fmt.Fprintf(os.Stderr, "Error preparing for capture: %s\n", err)
 		os.Exit(1)
 	}
 	os.Exit(0)
-}
-
-func prepareForCapture(client *srpc.Client, streamName string) error {
-	request := proto.PrepareForCaptureRequest{streamName}
-	var reply proto.PrepareForCaptureResponse
-	return client.RequestReply("ImageUnpacker.PrepareForCapture", request,
-		&reply)
 }
