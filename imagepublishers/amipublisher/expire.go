01f0d25e2188146403760239cb5e90405df39f00
Richard Gooch <rg+github0@safe-mbox.com>
2017-01-01 07:40:50 -0800

    Add and use imagepublishers/amipublisher.forEachAccountAndRegion().

--- imagepublishers/amipublisher/expire.go
+++ imagepublishers/amipublisher/expire.go
@@ -2,48 +2,30 @@ package amipublisher
 
 import (
 	"github.com/Symantec/Dominator/lib/log"
-	"github.com/Symantec/Dominator/lib/log/prefixlogger"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
-	"sync"
 	"time"
 )
 
 const expiresAtFormat = "2006-01-02:15:04:05"
 
 func expireResources(accountNames []string, logger log.Logger) error {
-	waitGroup := &sync.WaitGroup{}
 	currentTime := time.Now() // Need a common "now" time.
-	for _, accountName := range accountNames {
-		awsSession, err := createSession(accountName)
-		if err != nil {
-			return err
-		}
-		aRegionName := "us-east-1"
-		aAwsService := createService(awsSession, aRegionName)
-		regions, err := listRegions(aAwsService)
-		if err != nil {
-			return err
-		}
-		for _, region := range regions {
-			awsService := aAwsService
-			if region != aRegionName {
-				awsService = createService(awsSession, region)
-			}
-			logger := prefixlogger.New(accountName+": "+region+": ", logger)
-			waitGroup.Add(1)
-			go expireRegionResources(awsService, waitGroup, currentTime,
-				logger)
-		}
-
+	waitChannel := make(chan struct{})
+	numTargets, err := forEachAccountAndRegion(accountNames, nil,
+		func(awsService *ec2.EC2, account, region string, logger log.Logger) {
+			expireRegionResources(awsService, currentTime, logger)
+			waitChannel <- struct{}{}
+		},
+		logger)
+	for i := 0; i < numTargets; i++ {
+		<-waitChannel
 	}
-	waitGroup.Wait()
-	return nil
+	return err
 }
 
-func expireRegionResources(awsService *ec2.EC2, waitGroup *sync.WaitGroup,
-	currentTime time.Time, logger log.Logger) {
-	defer waitGroup.Done()
+func expireRegionResources(awsService *ec2.EC2, currentTime time.Time,
+	logger log.Logger) {
 	filters := make([]*ec2.Filter, 1)
 	values := make([]string, 1)
 	values[0] = "ExpiresAt"
--- imagepublishers/amipublisher/iterators.go
+++ imagepublishers/amipublisher/iterators.go
@@ -1,11 +1,17 @@
 package amipublisher
 
 import (
+	"errors"
 	"github.com/Symantec/Dominator/lib/log"
 	"github.com/Symantec/Dominator/lib/log/prefixlogger"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
+
+type resultsType struct {
+	numTargets int
+	err        error
+}
 
 func forEachResource(resources []Resource, stopOnError bool,
 	resourceFunc func(*ec2.EC2, Resource, log.Logger) error,
@@ -47,4 +53,69 @@ func forEachResource(resources []Resource, stopOnError bool,
 		}
 	}
 	return firstError
+}
+
+func forEachAccountAndRegion(accountProfileNames []string, regions []string,
+	targetFunc func(*ec2.EC2, string, string, log.Logger),
+	logger log.Logger) (int, error) {
+	if len(accountProfileNames) < 1 {
+		return 0, errors.New("no account names")
+	}
+	logger.Println("Creating sessions...")
+	awsSessions := make(map[string]*session.Session, len(accountProfileNames))
+	for _, accountProfileName := range accountProfileNames {
+		awsSession, err := createSession(accountProfileName)
+		if err != nil {
+			return 0, err
+		}
+		awsSessions[accountProfileName] = awsSession
+	}
+	logger.Println("Starting goroutines...")
+	accountResultsChannel := make(chan resultsType, 1)
+	var numTargets int
+	for _, accountProfileName := range accountProfileNames {
+		go forEachRegionInAccount(awsSessions[accountProfileName],
+			accountProfileName, regions, accountResultsChannel, targetFunc,
+			logger)
+	}
+	var firstError error
+	// Collect account results.
+	for range accountProfileNames {
+		result := <-accountResultsChannel
+		if result.err != nil && firstError == nil {
+			firstError = result.err
+		}
+		numTargets += result.numTargets
+	}
+	return numTargets, firstError
+}
+
+func forEachRegionInAccount(awsSession *session.Session,
+	accountProfileName string, regions []string,
+	resultsChannel chan<- resultsType,
+	targetFunc func(*ec2.EC2, string, string, log.Logger),
+	logger log.Logger) {
+	aRegionName := "us-east-1"
+	var aAwsService *ec2.EC2
+	if len(regions) < 1 {
+		var err error
+		aAwsService := createService(awsSession, aRegionName)
+		regions, err = listRegions(aAwsService)
+		if err != nil {
+			resultsChannel <- resultsType{0, err}
+			return
+		}
+	}
+	// Start goroutine for each target ((account,region) tuple).
+	for _, region := range regions {
+		logger := prefixlogger.New(accountProfileName+": "+region+": ", logger)
+		var awsService *ec2.EC2
+		if region == aRegionName && aAwsService != nil {
+			awsService = aAwsService
+		} else {
+			awsService = createService(awsSession, region)
+		}
+		go targetFunc(awsService, accountProfileName, region, logger)
+	}
+	resultsChannel <- resultsType{len(regions), nil}
 }
