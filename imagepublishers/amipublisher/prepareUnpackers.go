61129583462d67a395c39b1acdf2292e6d424b89
Richard Gooch <rg+github0@safe-mbox.com>
2017-01-02 07:25:10 -0800

    Use forEachAccountAndRegion() in amipublisher.PrepareUnpackers().

--- imagepublishers/amipublisher/prepareUnpackers.go
+++ imagepublishers/amipublisher/prepareUnpackers.go
@@ -5,9 +5,7 @@ import (
 	uclient "github.com/Symantec/Dominator/imageunpacker/client"
 	"github.com/Symantec/Dominator/lib/constants"
 	"github.com/Symantec/Dominator/lib/log"
-	"github.com/Symantec/Dominator/lib/log/prefixlogger"
 	"github.com/Symantec/Dominator/lib/srpc"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"strconv"
 	"time"
@@ -15,34 +13,18 @@ import (
 
 func prepareUnpackers(streamName string, accountProfileNames []string,
 	regions []string, logger log.Logger) error {
-	if len(accountProfileNames) < 1 {
-		return errors.New("no account names")
-	}
-	logger.Println("Creating sessions...")
-	accountResultsChannel := make(chan accountResult, 1)
 	resultsChannel := make(chan error, 1)
-	for _, accountProfileName := range accountProfileNames {
-		awsSession, err := createSession(accountProfileName)
-		if err != nil {
-			return err
-		}
-		go prepareUnpackersInAccount(awsSession, streamName, accountProfileName,
-			regions, accountResultsChannel, resultsChannel, logger)
-		if err != nil {
-			return err
-		}
-	}
-	var numTargets int
-	// Collect account results.
-	for range accountProfileNames {
-		result := <-accountResultsChannel
-		if result.err != nil {
-			return result.err
-		}
-		numTargets += result.numRegions
-	}
+	numTargets, err := forEachAccountAndRegion(accountProfileNames, regions,
+		func(awsService *ec2.EC2, account, region string, logger log.Logger) {
+			err := prepareUnpacker(awsService, streamName, logger)
+			if err != nil {
+				logger.Println(err)
+			}
+			resultsChannel <- err
+		},
+		logger)
 	// Collect results.
-	var firstError error
+	firstError := err
 	for i := 0; i < numTargets; i++ {
 		err := <-resultsChannel
 		if err != nil && firstError == nil {
@@ -50,43 +32,6 @@ func prepareUnpackers(streamName string, accountProfileNames []string,
 		}
 	}
 	return firstError
-}
-
-func prepareUnpackersInAccount(awsSession *session.Session, streamName string,
-	accountProfileName string, regions []string,
-	accountResultsChannel chan<- accountResult, resultsChannel chan<- error,
-	logger log.Logger) {
-	aRegionName := "us-east-1"
-	var aAwsService *ec2.EC2
-	if len(regions) < 1 {
-		var err error
-		aAwsService := createService(awsSession, aRegionName)
-		regions, err = listRegions(aAwsService)
-		if err != nil {
-			accountResultsChannel <- accountResult{0, err}
-			return
-		}
-	}
-	// Start manager for each region.
-	numRegions := 0
-	for _, region := range regions {
-		logger := prefixlogger.New(accountProfileName+": "+region+": ", logger)
-		var awsService *ec2.EC2
-		if region == aRegionName && aAwsService != nil {
-			awsService = aAwsService
-		} else {
-			awsService = createService(awsSession, region)
-		}
-		numRegions++
-		go func() {
-			err := prepareUnpacker(awsService, streamName, logger)
-			if err != nil {
-				logger.Println(err)
-			}
-			resultsChannel <- err
-		}()
-	}
-	accountResultsChannel <- accountResult{numRegions, nil}
 }
 
 func prepareUnpacker(awsService *ec2.EC2, streamName string,
