824c9a8485842cc19133e57b8364766b80233009
Richard Gooch <rg+github0@safe-mbox.com>
2017-01-21 22:01:18 -0800

    Add launch-instances subcommand to ami-publisher.

---
+++ cmd/ami-publisher/launchInstances.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Symantec/Dominator/imagepublishers/amipublisher"
+	"github.com/Symantec/Dominator/lib/awsutil"
+	"github.com/Symantec/Dominator/lib/log"
+	"os"
+	"path"
+)
+
+func launchInstancesSubcommand(args []string, logger log.Logger) {
+	domImage := ""
+	if len(args) > 1 {
+		domImage = args[1]
+	}
+	err := launchInstances(args[0], domImage, logger)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error copying bootstrap images: %s\n", err)
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func launchInstances(bootImage, domImage string, logger log.Logger) error {
+	bootImage = path.Clean(bootImage)
+	tags, err := makeTags()
+	if err != nil {
+		return err
+	}
+	tags["Name"] = *unpackerName
+	if domImage != "" {
+		tags["RequiredImage"] = domImage
+	}
+	imageTags := make(awsutil.Tags)
+	for key, value := range searchTags {
+		imageTags[key] = value
+	}
+	imageTags["Name"] = bootImage
+	return amipublisher.LaunchInstances(targets, skipTargets, imageTags,
+		subnetSearchTags, vpcSearchTags, securityGroupSearchTags,
+		*instanceType, *sshKeyName, tags, logger)
+}
--- cmd/ami-publisher/main.go
+++ cmd/ami-publisher/main.go
@@ -22,25 +22,41 @@ var (
 		"Hostname of imageserver")
 	imageServerPortNum = flag.Uint("imageServerPortNum",
 		constants.ImageServerPortNumber, "Port number of imageserver")
+	instanceType = flag.String("instanceType", "t2.medium",
+		"Instance type to launch")
 	maxIdleTime = flag.Duration("maxIdleTime", time.Minute*50,
 		"Maximum idle time for image unpacker instances")
 	minFreeBytes = flag.Uint64("minFreeBytes", 1<<28,
 		"minimum number of free bytes in image")
-	skipTargets awsutil.TargetList
-	tagsFile    = flag.String("tagsFile", "",
+	searchTags              = awsutil.Tags{"Preferred": ""}
+	securityGroupSearchTags awsutil.Tags
+	skipTargets             awsutil.TargetList
+	sshKeyName              = flag.String("sshKeyName", "",
+		"Name of SSH key for instance")
+	subnetSearchTags awsutil.Tags = awsutil.Tags{"Network": "Private"}
+	tagsFile                      = flag.String("tagsFile", "",
 		"JSON encoded file containing tags to apply to AMIs")
 	tagKey       = flag.String("tagKey", "", "Tag key name to apply")
 	tagValue     = flag.String("tagValue", "", "Tag value to apply")
 	targets      awsutil.TargetList
 	unpackerName = flag.String("unpackerName", "ImageUnpacker",
 		"The Name tag value for image unpacker instances")
+	vpcSearchTags awsutil.Tags = awsutil.Tags{"Preferred": ""}
 )
 
 func init() {
+	flag.Var(&searchTags, "searchTags",
+		"Name of tags to use when searching for resources")
+	flag.Var(&securityGroupSearchTags, "securityGroupSearchTags",
+		"Restrict security group search to given tags")
 	flag.Var(&skipTargets, "skipTargets",
 		"List of targets to skip (default none). No wildcards permitted")
+	flag.Var(&subnetSearchTags, "subnetSearchTags",
+		"Restrict subnet search to given tags")
 	flag.Var(&targets, "targets",
 		"List of targets (default all accounts and regions)")
+	flag.Var(&vpcSearchTags, "vpcSearchTags",
+		"Restrict VPC search to given tags")
 }
 
 func printUsage() {
@@ -54,6 +70,7 @@ func printUsage() {
 	fmt.Fprintln(os.Stderr, "  delete-tag-on-unpackers tag-key")
 	fmt.Fprintln(os.Stderr, "  expire")
 	fmt.Fprintln(os.Stderr, "  import-key-pair name pub-key-file")
+	fmt.Fprintln(os.Stderr, "  launch-instances boot-image [dominator-image]")
 	fmt.Fprintln(os.Stderr, "  list-unpackers")
 	fmt.Fprintln(os.Stderr, "  prepare-unpackers stream-name")
 	fmt.Fprintln(os.Stderr, "  publish stream-name image-leaf-name")
@@ -78,6 +95,7 @@ var subcommands = []subcommand{
 	{"delete-tags-on-unpackers", 1, 1, deleteTagsOnUnpackersSubcommand},
 	{"expire", 0, 0, expireSubcommand},
 	{"import-key-pair", 2, 2, importKeyPairSubcommand},
+	{"launch-instances", 1, 2, launchInstancesSubcommand},
 	{"list-unpackers", 0, 0, listUnpackersSubcommand},
 	{"prepare-unpackers", 1, 1, prepareUnpackersSubcommand},
 	{"publish", 2, 2, publishSubcommand},
