c80cbb781f90957c047eb5c37c2c2a8614dbf56c
Richard Gooch <rg+github0@safe-mbox.com>
2017-01-28 06:36:02 -0800

    Add copy subcommand to imagetool.

---
+++ cmd/imagetool/copyImageSubcommand.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/Symantec/Dominator/imageserver/client"
+	"github.com/Symantec/Dominator/lib/srpc"
+	"os"
+)
+
+func copyImageSubcommand(args []string) {
+	imageSClient, _ := getClients()
+	if err := copyImage(imageSClient, args[0], args[1]); err != nil {
+		fmt.Fprintf(os.Stderr, "Error copying image: %s\n", err)
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func copyImage(imageSClient *srpc.Client, name, oldImageName string) error {
+	imageExists, err := client.CheckImage(imageSClient, name)
+	if err != nil {
+		return errors.New("error checking for image existance: " + err.Error())
+	}
+	if imageExists {
+		return errors.New("image exists")
+	}
+	image, err := getImage(imageSClient, oldImageName)
+	return addImage(imageSClient, name, image)
+}
