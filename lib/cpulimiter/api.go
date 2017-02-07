52da3af4e837222de3cfc601bfc9604fbd77381c
Richard Gooch <rg+github0@safe-mbox.com>
2017-02-07 11:02:53 -0800

    Reset calculations in lib/cpulimiter when limit changed.

--- lib/cpulimiter/api.go
+++ lib/cpulimiter/api.go
@@ -2,10 +2,12 @@ package cpulimiter
 
 import (
 	"github.com/Symantec/Dominator/lib/wsyscall"
+	"sync"
 	"time"
 )
 
 type CpuLimiter struct {
+	mutex            sync.Mutex
 	confCpuPercent   uint // Aggregate across all CPUs.
 	cpuPercent       uint // For a single CPU.
 	lastProbeTime    time.Time
@@ -20,7 +22,9 @@ func (cl *CpuLimiter) Limit() error {
 	return cl.limit()
 }
 
-func (cl *CpuLimiter) CpuPercent() uint { return cl.confCpuPercent }
+func (cl *CpuLimiter) CpuPercent() uint {
+	return cl.getConfCpuPercent()
+}
 
 func (cl *CpuLimiter) SetCpuPercent(cpuPercent uint) {
 	cl.setCpuPercent(cpuPercent)
