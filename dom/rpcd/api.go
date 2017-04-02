c93ce07adc6c14cb169a02ca1a0fa03fa2949b72
Richard Gooch <rg+github0@safe-mbox.com>
2017-04-02 07:04:08 -0700

    Change dom/rpcd.Setup() to use log.Logger interface type.

--- dom/rpcd/api.go
+++ dom/rpcd/api.go
@@ -2,16 +2,16 @@ package rpcd
 
 import (
 	"github.com/Symantec/Dominator/dom/herd"
+	"github.com/Symantec/Dominator/lib/log"
 	"github.com/Symantec/Dominator/lib/srpc"
-	"log"
 )
 
 type rpcType struct {
 	herd   *herd.Herd
-	logger *log.Logger
+	logger log.Logger
 }
 
-func Setup(herd *herd.Herd, logger *log.Logger) {
+func Setup(herd *herd.Herd, logger log.Logger) {
 	rpcObj := &rpcType{
 		herd:   herd,
 		logger: logger}
