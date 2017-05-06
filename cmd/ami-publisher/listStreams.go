aa42fc5119940966b3dee143312b6a21de08588d
Richard Gooch <rg+github0@safe-mbox.com>
2017-05-06 06:22:53 -0700

    Add list-streams subcommand to ami-publisher.

---
+++ cmd/ami-publisher/listStreams.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Symantec/Dominator/imagepublishers/amipublisher"
+	libjson "github.com/Symantec/Dominator/lib/json"
+	"github.com/Symantec/Dominator/lib/log"
+	"github.com/Symantec/Dominator/lib/verstr"
+
+	"os"
+)
+
+func listStreamsSubcommand(args []string, logger log.Logger) {
+	err := listStreams(logger)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error listing streams: %s\n", err)
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func listStreams(logger log.Logger) error {
+	results, err := amipublisher.ListStreams(targets, skipTargets,
+		*instanceName, logger)
+	if err != nil {
+		return err
+	}
+	streams := make([]string, 0, len(results))
+	for stream := range results {
+		streams = append(streams, stream)
+	}
+	verstr.Sort(streams)
+	return libjson.WriteWithIndent(os.Stdout, "    ", streams)
+}
--- cmd/ami-publisher/main.go
+++ cmd/ami-publisher/main.go
@@ -83,6 +83,7 @@ func printUsage() {
 	fmt.Fprintln(os.Stderr, "  import-key-pair name pub-key-file")
 	fmt.Fprintln(os.Stderr, "  launch-instances boot-image")
 	fmt.Fprintln(os.Stderr, "  launch-instances-for-images results-file...")
+	fmt.Fprintln(os.Stderr, "  list-streams")
 	fmt.Fprintln(os.Stderr, "  list-unpackers")
 	fmt.Fprintln(os.Stderr, "  prepare-unpackers [stream-name]")
 	fmt.Fprintln(os.Stderr, "  publish stream-name image-leaf-name")
@@ -113,6 +114,7 @@ var subcommands = []subcommand{
 	{"import-key-pair", 2, 2, importKeyPairSubcommand},
 	{"launch-instances", 1, 1, launchInstancesSubcommand},
 	{"launch-instances-for-images", 0, -1, launchInstancesForImagesSubcommand},
+	{"list-streams", 0, 0, listStreamsSubcommand},
 	{"list-unpackers", 0, 0, listUnpackersSubcommand},
 	{"prepare-unpackers", 0, 1, prepareUnpackersSubcommand},
 	{"publish", 2, 2, publishSubcommand},
