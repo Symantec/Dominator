ecfdf263b95bc634f14c6df06b95b1b85db3cccf
Richard Gooch <rg+github0@safe-mbox.com>
2017-07-10 06:34:58 -0700

    Check in lib/url/urlutil package.

---
+++ lib/url/urlutil/impl.go
@@ -0,0 +1,30 @@
+package urlutil
+
+import (
+	"errors"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+func open(rawurl string) (io.ReadCloser, error) {
+	u, err := url.Parse(rawurl)
+	if err != nil {
+		return nil, err
+	}
+	if u.Scheme == "file" {
+		return os.Open(u.Path)
+	}
+	if u.Scheme == "http" || u.Scheme == "https" {
+		resp, err := http.Get(rawurl)
+		if err != nil {
+			return nil, err
+		}
+		if resp.StatusCode != http.StatusOK {
+			return nil, errors.New(resp.Status)
+		}
+		return resp.Body, nil
+	}
+	return nil, errors.New("unknown scheme: " + u.Scheme)
+}
