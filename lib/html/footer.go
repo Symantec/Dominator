d767138b32db12a227e2f9c70e7e2d0d392e88a4
Richard Gooch <rg+github0@safe-mbox.com>
2018-01-09 06:47:10 -0800

    Show Go runtime version in HTML footer.

--- lib/html/footer.go
+++ lib/html/footer.go
@@ -3,12 +3,14 @@ package html
 import (
 	"fmt"
 	"io"
+	"runtime"
 	"time"
 
 	"github.com/Symantec/Dominator/lib/format"
 )
 
 func writeFooter(writer io.Writer) {
-	fmt.Fprintf(writer, "Page generated at: %s<br>\n",
-		time.Now().Format(format.TimeFormatSeconds))
+	fmt.Fprintf(writer, "Page generated at: %s with %s<br>\n",
+		time.Now().Format(format.TimeFormatSeconds),
+		runtime.Version())
 }
