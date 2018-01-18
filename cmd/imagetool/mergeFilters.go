907f9f34a8092f0d3b920b101d406c93e2c48e6b
Richard Gooch <rg+github0@safe-mbox.com>
2018-01-18 07:26:36 -0800

    Add merge-filters subcommand to imagetool.

---
+++ cmd/imagetool/mergeFilters.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Symantec/Dominator/lib/filter"
+)
+
+func mergeFiltersSubcommand(args []string) {
+	err := mergeFilters(args)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error merging filter: %s\n", err)
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func mergeFilters(filterFiles []string) error {
+	mergeableFilter := &filter.MergeableFilter{}
+	for _, filterFile := range filterFiles {
+		filt, err := filter.Load(filterFile)
+		if err != nil {
+			return err
+		}
+		mergeableFilter.Merge(filt)
+	}
+	filt := mergeableFilter.ExportFilter()
+	for _, filterLine := range filt.FilterLines {
+		if _, err := fmt.Println(filterLine); err != nil {
+			return err
+		}
+	}
+	return nil
+}
