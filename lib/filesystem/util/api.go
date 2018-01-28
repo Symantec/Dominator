fd4395b607c424a36afe745a4efdc672cb624187
Richard Gooch <rg+github0@safe-mbox.com>
2018-01-28 13:32:34 -0800

    Add lib/filesystem/util.ReplaceComputedFiles() function.

--- lib/filesystem/util/api.go
+++ lib/filesystem/util/api.go
@@ -11,6 +11,11 @@ type ComputedFile struct {
 	Source   string
 }
 
+type ComputedFilesData struct {
+	FileData      map[string][]byte // Key: filename.
+	RootDirectory string
+}
+
 // CopyMtimes will copy modification times for files from the source to the
 // destination if the file data and metadata (other than mtime) are identical.
 // Directory entry inode pointers are invalidated by this operation, so this
@@ -21,6 +26,13 @@ func CopyMtimes(source, dest *filesystem.FileSystem) {
 
 func LoadComputedFiles(filename string) ([]ComputedFile, error) {
 	return loadComputedFiles(filename)
+}
+
+func ReplaceComputedFiles(fs *filesystem.FileSystem,
+	computedFilesData *ComputedFilesData,
+	objectsGetter objectserver.ObjectsGetter) (
+	objectserver.ObjectsGetter, error) {
+	return replaceComputedFiles(fs, computedFilesData, objectsGetter)
 }
 
 func SpliceComputedFiles(fs *filesystem.FileSystem,
