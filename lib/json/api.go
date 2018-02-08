588e86c328bd9eb3bc3630a89333363d57829641
Richard Gooch <rg+github0@safe-mbox.com>
2018-02-08 06:51:13 -0800

    Add lib/json.WriteToFile() function.

--- lib/json/api.go
+++ lib/json/api.go
@@ -2,10 +2,16 @@ package json
 
 import (
 	"io"
+	"os"
 )
 
 func ReadFromFile(filename string, value interface{}) error {
 	return readFromFile(filename, value)
+}
+
+func WriteToFile(filename string, perm os.FileMode, indent string,
+	value interface{}) error {
+	return writeToFile(filename, perm, indent, value)
 }
 
 func WriteWithIndent(w io.Writer, indent string, value interface{}) error {
