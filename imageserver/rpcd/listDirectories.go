99fc906d8b70ef196017e2df31549244fda4688b
Richard Gooch <rg+github0@safe-mbox.com>
2018-02-28 07:45:20 -0800

    Switch imageserver/rpcd to coder method types.

--- imageserver/rpcd/listDirectories.go
+++ imageserver/rpcd/listDirectories.go
@@ -1,14 +1,12 @@
 package rpcd
 
 import (
-	"encoding/gob"
-
 	"github.com/Symantec/Dominator/lib/image"
 	"github.com/Symantec/Dominator/lib/srpc"
 )
 
-func (t *srpcType) ListDirectories(conn *srpc.Conn) error {
-	encoder := gob.NewEncoder(conn)
+func (t *srpcType) ListDirectories(conn *srpc.Conn, decoder srpc.Decoder,
+	encoder srpc.Encoder) error {
 	for _, directory := range t.imageDataBase.ListDirectories() {
 		if err := encoder.Encode(directory); err != nil {
 			return err
