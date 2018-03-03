f0eb91700746ccf43ffae4df36fa6836c007a451
Richard Gooch <rg+github0@safe-mbox.com>
2018-03-03 07:18:17 -0800

    Switch ObjectServer.AddObjects SRPC methods to coder types.

--- objectserver/rpcd/addObjects.go
+++ objectserver/rpcd/addObjects.go
@@ -7,11 +7,12 @@ import (
 	"github.com/Symantec/Dominator/objectserver/rpcd/lib"
 )
 
-func (t *srpcType) AddObjects(conn *srpc.Conn) error {
+func (t *srpcType) AddObjects(conn *srpc.Conn, decoder srpc.Decoder,
+	encoder srpc.Encoder) error {
 	defer runtime.GC() // An opportune time to take out the garbage.
 	if t.replicationMaster == "" {
-		return lib.AddObjects(conn, t.objectServer, t.logger)
+		return lib.AddObjects(conn, decoder, encoder, t.objectServer, t.logger)
 	}
-	return lib.AddObjectsWithMaster(conn, t.objectServer, t.replicationMaster,
-		t.logger)
+	return lib.AddObjectsWithMaster(conn, decoder, encoder, t.objectServer,
+		t.replicationMaster, t.logger)
 }
