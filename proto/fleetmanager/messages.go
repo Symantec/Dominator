d56ab0c0e77d8e9b9199ec6cdf241d3622d20793
Richard Gooch <rg+github0@safe-mbox.com>
2018-05-24 07:16:11 -0700

    Add location querying messages to proto/fleetmanager package.

--- proto/fleetmanager/messages.go
+++ proto/fleetmanager/messages.go
@@ -12,3 +12,32 @@ type GetHypervisorForVMResponse struct {
 	HypervisorAddress string // host:port
 	Error             string
 }
+
+type ListHypervisorLocationsRequest struct {
+	TopLocation string
+}
+
+type ListHypervisorLocationsResponse struct {
+	Locations []string
+	Error     string
+}
+
+type ListHypervisorsInLocationRequest struct {
+	Location string
+}
+
+type ListHypervisorsInLocationResponse struct {
+	HypervisorAddresses []string // host:port
+	Error               string
+}
+
+type ListVMsInLocationRequest struct {
+	Location string
+}
+
+// A stream of ListVMsInLocationResponse messages is sent, until either the
+// length of the IpAddresses field is zero, or the Error field != "".
+type ListVMsInLocationResponse struct {
+	IpAddresses []net.IP
+	Error       string
+}
