60ca1da1965add97cf743d1f33cc69f77df00f64
Richard Gooch <rg+github0@safe-mbox.com>
2018-07-08 10:39:34 -0700

    Check in lib/srpc/serverutil package.

---
+++ lib/srpc/serverutil/api.go
@@ -0,0 +1,28 @@
+package serverutil
+
+import (
+	"sync"
+
+	"github.com/Symantec/Dominator/lib/srpc"
+)
+
+type PerUserMethodLimiter struct {
+	mutex               sync.Mutex
+	perUserMethodCounts map[userMethodType]uint
+	perUserMethodLimits map[string]uint
+}
+
+type userMethodType struct {
+	method   string
+	username string
+}
+
+func NewPerUserMethodLimiter(
+	perUserMethodLimits map[string]uint) *PerUserMethodLimiter {
+	return newPerUserMethodLimiter(perUserMethodLimits)
+}
+
+func (limiter *PerUserMethodLimiter) BlockMethod(methodName string,
+	authInfo *srpc.AuthInformation) (func(), error) {
+	return limiter.blockMethod(methodName, authInfo)
+}
