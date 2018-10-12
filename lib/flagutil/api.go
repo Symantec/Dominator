867e0e6a1d6095eb8acadaeff4fabf90798b3281
Richard Gooch <rg+github0@safe-mbox.com>
2018-10-12 11:44:49 -0700

    Add lib/flagutil.Size type.

--- lib/flagutil/api.go
+++ lib/flagutil/api.go
@@ -6,6 +6,10 @@
 */
 package flagutil
 
+// A Size is an integer number of bytes that satisfies the standard library
+// flag.Value interface.
+type Size uint64
+
 // A StringList is a slice of strings that satisfies the standard library
 // flag.Value interface.
 type StringList []string
