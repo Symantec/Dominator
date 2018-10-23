c4489ba12fc5a0d344438f58dd5ee0b8c4993847
Richard Gooch <rg+github0@safe-mbox.com>
2018-10-23 12:29:51 -0700

    Check in installer protocol messages.

---
+++ proto/installer/messages.go
@@ -0,0 +1,19 @@
+package installer
+
+const (
+	FileSystemTypeExt4 = 0
+)
+
+type FileSystemType uint
+
+type Partition struct {
+	FileSystemType   FileSystemType `json:",omitempty"`
+	MountPoint       string         `json:",omitempty"`
+	MinimumFreeBytes uint64         `json:",omitempty"`
+}
+
+type StorageLayout struct {
+	BootDriveLayout          []Partition `json:",omitempty"`
+	ExtraMountPointsBasename string      `json:",omitempty"`
+	Encrypt                  bool        `json:",omitempty"`
+}
