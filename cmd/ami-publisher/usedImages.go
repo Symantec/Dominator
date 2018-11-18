9f72ba427dab5ef1d2d3b19ecd74af293bb39e07
Richard Gooch <rg+github0@safe-mbox.com>
2018-11-18 09:48:45 -0800

    Add list-used-images subcommand to ami-publisher tool.

---
+++ cmd/ami-publisher/usedImages.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Symantec/Dominator/imagepublishers/amipublisher"
+	libjson "github.com/Symantec/Dominator/lib/json"
+	"github.com/Symantec/Dominator/lib/log"
+)
+
+func listUsedImagesSubcommand(args []string, logger log.DebugLogger) {
+	err := listUsedImages(logger)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error listing used images: %s\n", err)
+		os.Exit(1)
+	}
+	logMemoryUsage(logger)
+	os.Exit(0)
+}
+
+func listUsedImages(logger log.DebugLogger) error {
+	results, err := amipublisher.ListUsedImages(targets, skipTargets,
+		searchTags, excludeSearchTags, logger)
+	if err != nil {
+		return err
+	}
+	if err := libjson.WriteWithIndent(os.Stdout, "    ", results); err != nil {
+		return err
+	}
+	return nil
+}
