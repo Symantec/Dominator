b162bd76d621dca3f4f20cfa03dee6014b17f94a
Richard Gooch <rg+github0@safe-mbox.com>
2018-12-29 08:24:35 -0800

    Add hypervisor/client.GetVmInfo() function.

--- hypervisor/client/api.go
+++ hypervisor/client/api.go
@@ -4,6 +4,7 @@ import (
 	"net"
 
 	"github.com/Symantec/Dominator/lib/srpc"
+	proto "github.com/Symantec/Dominator/proto/hypervisor"
 )
 
 func DeleteVmVolume(client *srpc.Client, ipAddr net.IP, accessToken []byte,
@@ -13,6 +14,10 @@ func DeleteVmVolume(client *srpc.Client, ipAddr net.IP, accessToken []byte,
 
 func DestroyVm(client *srpc.Client, ipAddr net.IP, accessToken []byte) error {
 	return destroyVm(client, ipAddr, accessToken)
+}
+
+func GetVmInfo(client *srpc.Client, ipAddr net.IP) (proto.VmInfo, error) {
+	return getVmInfo(client, ipAddr)
 }
 
 func PrepareVmForMigration(client *srpc.Client, ipAddr net.IP,
