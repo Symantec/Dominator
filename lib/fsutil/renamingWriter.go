a032cf99ff4f64bfbf4b6d47cf9894f0c87b2040
Richard Gooch <rg+github0@safe-mbox.com>
2019-06-24 08:12:32 -0700

    Add Abort() and fsync(2) attempts to lib/fsutil.RenamingWriter.

--- lib/fsutil/renamingWriter.go
+++ lib/fsutil/renamingWriter.go
@@ -2,7 +2,10 @@ package fsutil
 
 import (
 	"os"
+	"time"
 )
+
+var fsyncSemaphore = make(chan struct{}, 1)
 
 func createRenamingWriter(filename string, perm os.FileMode) (
 	*RenamingWriter, error) {
@@ -17,9 +20,33 @@ func createRenamingWriter(filename string, perm os.FileMode) (
 	return writer, nil
 }
 
+// fsyncFile will call file.Sync if it has not been called recently. This
+// attempts to reduce the performance problems of fsync(2) by potentially
+// sacrificing some file-system consistency.
+func fsyncFile(file *os.File) error {
+	select {
+	case fsyncSemaphore <- struct{}{}:
+	default:
+		return nil
+	}
+	startTime := time.Now()
+	err := file.Sync()
+	duration := time.Since(startTime)
+	go func() {
+		time.Sleep(duration)
+		<-fsyncSemaphore
+	}()
+	return err
+}
+
 func (w *RenamingWriter) close() error {
 	tmpFilename := w.filename + "~"
 	defer os.Remove(tmpFilename)
+	if !w.abort {
+		if err := fsyncFile(w.File); err != nil {
+			return err
+		}
+	}
 	if err := w.File.Close(); err != nil {
 		return err
 	}
