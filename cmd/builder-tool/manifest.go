ea2d39198c4a6be39d78143912d89ce38faa6d0f
Richard Gooch <rg+github0@safe-mbox.com>
2019-08-15 07:39:14 -0700

    Support -alwaysShowBuildLog option in builder-tool build-from-manifest.

--- cmd/builder-tool/manifest.go
+++ cmd/builder-tool/manifest.go
@@ -16,15 +16,28 @@ import (
 
 const filePerms = syscall.S_IRUSR | syscall.S_IRGRP | syscall.S_IROTH
 
+type logWriterType struct {
+	buffer bytes.Buffer
+}
+
 func buildFromManifestSubcommand(args []string, logger log.DebugLogger) {
 	srpcClient := getImageServerClient()
-	buildLog := &bytes.Buffer{}
+	logWriter := &logWriterType{}
+	if *alwaysShowBuildLog {
+		fmt.Fprintln(os.Stderr, "Start of build log ==========================")
+	}
 	name, err := builder.BuildImageFromManifest(srpcClient, args[0], args[1],
-		*expiresIn, buildLog, logger)
+		*expiresIn, logWriter, logger)
 	if err != nil {
+		if !*alwaysShowBuildLog {
+			os.Stderr.Write(logWriter.Bytes())
+		}
+		fmt.Fprintln(os.Stderr, "End of build log ============================")
 		fmt.Fprintf(os.Stderr, "Error processing manifest: %s\n", err)
-		io.Copy(os.Stderr, buildLog)
 		os.Exit(1)
+	}
+	if *alwaysShowBuildLog {
+		fmt.Fprintln(os.Stderr, "End of build log ============================")
 	}
 	fmt.Println(name)
 	os.Exit(0)
@@ -60,4 +73,15 @@ func processManifestSubcommand(args []string, logger log.DebugLogger) {
 		io.Copy(os.Stdout, buildLog)
 	}
 	os.Exit(0)
+}
+
+func (w *logWriterType) Bytes() []byte {
+	return w.buffer.Bytes()
+}
+
+func (w *logWriterType) Write(p []byte) (int, error) {
+	if *alwaysShowBuildLog {
+		os.Stderr.Write(p)
+	}
+	return w.buffer.Write(p)
 }
