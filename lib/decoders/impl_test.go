477559028e10bfd0efdbe9b98d207c6f9419b571
Richard Gooch <rg+github0@safe-mbox.com>
2019-10-30 08:22:12 -0700

    Add unit tests to lib/decoders.

---
+++ lib/decoders/impl_test.go
@@ -0,0 +1,35 @@
+package decoders
+
+import (
+	"testing"
+)
+
+type testdataType struct {
+	Name  string
+	Value string
+}
+
+func checkTestdata(t *testing.T, testdata testdataType) {
+	if testdata.Name != "a name" {
+		t.Fatalf("Name field: \"%s\" != \"a name\"", testdata.Name)
+	}
+	if testdata.Value != "a value" {
+		t.Fatalf("Value field: \"%s\" != \"a value\"", testdata.Value)
+	}
+}
+
+func TestGobExplicit(t *testing.T) {
+	var testdata testdataType
+	if err := DecodeFile("testdata/data.gob", &testdata); err != nil {
+		t.Fatal(err)
+	}
+	checkTestdata(t, testdata)
+}
+
+func TestJsonExplicit(t *testing.T) {
+	var testdata testdataType
+	if err := DecodeFile("testdata/data.json", &testdata); err != nil {
+		t.Fatal(err)
+	}
+	checkTestdata(t, testdata)
+}
